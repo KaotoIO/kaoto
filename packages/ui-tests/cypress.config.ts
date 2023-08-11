3d0b4e28ab5afd14f65f858fb6381a9709776c4a
tplevko <tplevko@redhat.com>
2023-08-11 13:36:02 +0200

    Basic cypress e2e configuration

---
+++ packages/ui-tests/cypress.config.ts
@@ -0,0 +1,13 @@
+import { defineConfig } from 'cypress';
+
+export default defineConfig({
+  projectId: 'ui-test',
+  videoUploadOnPasses: false,
+
+  e2e: {
+    baseUrl: 'http://localhost:5173',
+    specPattern: 'cypress/e2e/**/*.{js,jsx,ts,tsx}',
+    viewportWidth: 1920,
+    viewportHeight: 1080,
+  },
+});
