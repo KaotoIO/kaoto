c9ba85bb5e76d3b046d956685a5bc6703a3d6925
Ricardo M <lordrip@gmail.com>
2023-08-22 14:13:06 +0200

    feat(camel-entities): Camel entities store model

--- packages/ui/src/models/index.ts
+++ packages/ui/src/models/index.ts
@@ -2,7 +2,6 @@ export * from './camel-catalog-index';
 export * from './camel-components-catalog';
 export * from './camel-processors-catalog';
 export * from './catalog-kind';
-export * from './flow';
 export * from './kamelets-catalog';
 export * from './react-component';
 export * from './schema';
