d85d024bab5f37d1684f673d4bc1ecea16398e1a
Ricardo M <lordrip@gmail.com>
2023-09-12 12:11:00 +0200

    fix(source-code): Add resolved URI to schema definition
    monaco-yaml requires to have a valid URI to fetch the appropiate schema
    for the monaco editor and also it uses the URI to determine the schema
    filename.
    
    This commit uses the resolved URI from the fetch call when loading
    the schema to leverage relative paths when applicable, otherwise, deploying
    kaoto-next in environments like GitHub Pages in which the deployment
    is not at the root path, it will fail since it will use the wrong
    URI.
    
    fixes: https://github.com/KaotoIO/kaoto-next/issues/109

--- packages/ui/src/providers/catalog-schema-loader.provider.tsx
+++ packages/ui/src/providers/catalog-schema-loader.provider.tsx
@@ -18,17 +18,17 @@ export const CatalogSchemaLoaderProvider: FunctionComponent<PropsWithChildren> =
     fetch(catalogIndex)
       .then((response) => response.json())
       .then((catalogIndex: CamelCatalogIndex) => {
-        const camelComponentsFiles = fetchCatalogFile(catalogIndex.catalogs.components.file);
-        const camelProcessorsFiles = fetchCatalogFile(catalogIndex.catalogs.models.file);
-        const kameletsFiles = fetchCatalogFile(catalogIndex.catalogs.kamelets.file);
+        const camelComponentsFiles = fetchFile(catalogIndex.catalogs.components.file);
+        const camelProcessorsFiles = fetchFile(catalogIndex.catalogs.models.file);
+        const kameletsFiles = fetchFile(catalogIndex.catalogs.kamelets.file);
 
         const schemaFiles = getSchemasFiles(catalogIndex.schemas);
 
         Promise.all([camelComponentsFiles, camelProcessorsFiles, kameletsFiles, Promise.all(schemaFiles)]).then(
           ([camelComponents, camelProcessors, kamelets, schemas]) => {
-            setCatalog(CatalogKind.Component, camelComponents);
-            setCatalog(CatalogKind.Processor, camelProcessors);
-            setCatalog(CatalogKind.Kamelet, kamelets);
+            setCatalog(CatalogKind.Component, camelComponents.body);
+            setCatalog(CatalogKind.Processor, camelProcessors.body);
+            setCatalog(CatalogKind.Kamelet, kamelets.body);
 
             CamelSchemasProcessor.getSchemas(schemas).forEach(setSchema);
 
@@ -45,22 +45,24 @@ export const CatalogSchemaLoaderProvider: FunctionComponent<PropsWithChildren> =
   );
 };
 
-async function fetchCatalogFile(file: string) {
+async function fetchFile(file: string) {
   /** The `.` is required to support relative routes in GitHub pages */
   const response = await fetch(`.${DEFAULT_CATALOG_PATH}/${file}`);
-  return await response.json();
+  const body = await response.json();
+
+  return { body, uri: response.url };
 }
 
 function getSchemasFiles(schemaFiles: CatalogEntry[]): Promise<Schema>[] {
   return schemaFiles.map(async (schemaDef) => {
-    const schema = await fetchCatalogFile(schemaDef.file);
+    const fetchedSchema = await fetchFile(schemaDef.file);
 
     return {
       name: schemaDef.name,
       tags: [],
       version: schemaDef.version,
-      uri: `${DEFAULT_CATALOG_PATH}/${schemaDef.file}`,
-      schema: schema,
+      uri: fetchedSchema.uri,
+      schema: fetchedSchema.body,
     };
   });
 }
