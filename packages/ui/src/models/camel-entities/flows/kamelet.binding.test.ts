375dfcb1941fda7d0861e9ac467c3370c8c23448
Ricardo M <lordrip@gmail.com>
2023-09-19 18:22:18 +0200

    chore: Split KameletBinding from routes.ts

--- packages/ui/src/models/camel-entities/flows/kamelet.binding.test.ts
+++ packages/ui/src/models/camel-entities/flows/kamelet.binding.test.ts
@@ -1,26 +1,5 @@
 import { EntityType } from '../base-entity';
-import { CamelRoute, KameletBinding } from './routes';
-
-describe('Camel Route', () => {
-  let camelEntity: CamelRoute;
-
-  beforeEach(() => {
-    camelEntity = new CamelRoute();
-  });
-
-  it('should have an uuid', () => {
-    expect(camelEntity.id).toBeDefined();
-    expect(typeof camelEntity.id).toBe('string');
-  });
-
-  it('should have a type', () => {
-    expect(camelEntity.type).toEqual(EntityType.Route);
-  });
-
-  it('should return the steps', () => {
-    expect(camelEntity.getSteps()).toEqual([]);
-  });
-});
+import { KameletBinding } from './kamelet-binding';
 
 describe('Kamelet Binding', () => {
   let kameletBinding: KameletBinding;
---
+++ packages/ui/src/models/camel-entities/flows/route.test.ts
@@ -0,0 +1,23 @@
+import { EntityType } from '../base-entity';
+import { CamelRoute } from './route';
+
+describe('Camel Route', () => {
+  let camelEntity: CamelRoute;
+
+  beforeEach(() => {
+    camelEntity = new CamelRoute();
+  });
+
+  it('should have an uuid', () => {
+    expect(camelEntity.id).toBeDefined();
+    expect(typeof camelEntity.id).toBe('string');
+  });
+
+  it('should have a type', () => {
+    expect(camelEntity.type).toEqual(EntityType.Route);
+  });
+
+  it('should return the steps', () => {
+    expect(camelEntity.getSteps()).toEqual([]);
+  });
+});
