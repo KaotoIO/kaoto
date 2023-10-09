b3d905fe91246de7983ff0a47ace272779040103
tplevko <tplevko@redhat.com>
2023-10-09 13:15:55 +0200

    feat: Improve component's icon visualization

--- packages/ui/src/components/Visualization/Custom/CustomNode.tsx
+++ packages/ui/src/components/Visualization/Custom/CustomNode.tsx
@@ -1,20 +1,30 @@
-import { CubeIcon } from '@patternfly/react-icons';
 import { DefaultNode, Node, WithSelectionProps, withSelection } from '@patternfly/react-topology';
 import { FunctionComponent } from 'react';
+import defaultCamelIcon from '../../../assets/camel-logo.svg';
 import { CanvasNode } from '../Canvas/canvas.models';
+import { IVisualizationNode } from '../../../models/visualization/base-visual-entity';
 
 interface CustomNodeProps extends WithSelectionProps {
   element: Node<CanvasNode, CanvasNode['data']>;
 }
 
+function getIcon(data: IVisualizationNode | undefined) {
+  const iconRadius = 20;
+  const cx = 25 / 2;
+  const cy = 25 / 2;
+
+  const icon = data?.iconData ?? defaultCamelIcon;
+  return (
+    <image xlinkHref={icon} x={cx - iconRadius} y={cy - iconRadius} width={iconRadius * 2} height={iconRadius * 2} />
+  );
+}
+
 const CustomNode: FunctionComponent<CustomNodeProps> = ({ element, ...rest }) => {
-  // const vizNode = element.getData()?.vizNode;
+  const data = element.getData()?.vizNode;
 
   return (
     <DefaultNode element={element} showStatusDecorator {...rest}>
-      <g transform={`translate(25, 25)`}>
-        <CubeIcon style={{ color: '#393F44' }} width={25} height={25} />
-      </g>
+      <g transform={`translate(25, 25)`}>{getIcon(data)}</g>
     </DefaultNode>
   );
 };
