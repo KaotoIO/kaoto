1cd996fb53a89ef41b23ba4b84c7b2e27095516f
Tomohisa Igarashi <tm.igarashi@gmail.com>
2023-10-30 15:19:20 -0400

    feat: Support configuring /PipeSpec/ErrorHandler
    Fixes: #188

--- packages/ui/src/pages/PipeErrorHandler/PipeErrorHandlerPage.tsx
+++ packages/ui/src/pages/PipeErrorHandler/PipeErrorHandlerPage.tsx
@@ -1,10 +1,10 @@
 import { TextContent, Title } from '@patternfly/react-core';
 import { FunctionComponent, useCallback, useContext, useMemo } from 'react';
-import { MetadataEditor } from '../../components/MetadataEditor';
 import { EntitiesContext } from '../../providers/entities.provider';
 import { PipeErrorHandler as PipeErrorHandlerType } from '@kaoto-next/camel-catalog/types';
 import { PipeResource, SourceSchemaType } from '../../models/camel';
 import { useSchemasStore } from '../../store';
+import { PipeErrorHandlerEditor } from '../../components/MetadataEditor/PipeErrorHandlerEditor';
 
 export const PipeErrorHandlerPage: FunctionComponent = () => {
   const schemaMap = useSchemasStore((state) => state.schemas);
@@ -43,10 +43,9 @@ export const PipeErrorHandlerPage: FunctionComponent = () => {
   return isSupported ? (
     <>
       <Title headingLevel="h1">Pipe ErrorHandler Configuration</Title>
-      <MetadataEditor
-        name="ErrorHandler"
+      <PipeErrorHandlerEditor
         schema={errorHandlerSchema}
-        metadata={getErrorHandlerModel()}
+        model={getErrorHandlerModel()}
         onChangeModel={onChangeModel}
       />
     </>
