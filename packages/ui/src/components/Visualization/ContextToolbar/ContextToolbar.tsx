4bf880175536ecdc261c14e51035212bdab40eb7
Ricardo M <lordrip@gmail.com>
2023-11-27 20:20:30 +0100

    chore(canvas): Increase Flow type tag size

--- packages/ui/src/components/Visualization/ContextToolbar/ContextToolbar.tsx
+++ packages/ui/src/components/Visualization/ContextToolbar/ContextToolbar.tsx
@@ -1,7 +1,8 @@
-import { Chip, ToolbarItem } from '@patternfly/react-core';
+import { ToolbarItem } from '@patternfly/react-core';
 import { FunctionComponent, useContext } from 'react';
 import { sourceSchemaConfig } from '../../../models/camel';
 import { EntitiesContext } from '../../../providers/entities.provider';
+import './ContextToolbar.scss';
 import { FlowClipboard } from './FlowClipboard/FlowClipboard';
 import { FlowExportImage } from './FlowExportImage/FlowExportImage';
 import { NewFlow } from './FlowType/NewFlow';
@@ -9,19 +10,18 @@ import { FlowsMenu } from './Flows/FlowsMenu';
 
 export const ContextToolbar: FunctionComponent = () => {
   const { currentSchemaType } = useContext(EntitiesContext)!;
+
   return [
-    <ToolbarItem key={'toolbar-current-dsl'}>
-      <Chip data-testid="toolbar-current-dsl" isReadOnly>
-        {sourceSchemaConfig.config[currentSchemaType].name || 'None'}
-      </Chip>
+    <ToolbarItem className="current-dsl-tag" key="toolbar-current-dsl">
+      <span data-testid="toolbar-current-dsl">{sourceSchemaConfig.config[currentSchemaType].name || 'None'}</span>
     </ToolbarItem>,
-    <ToolbarItem key={'toolbar-flows-list'}>
+    <ToolbarItem key="toolbar-flows-list">
       <FlowsMenu />
     </ToolbarItem>,
-    <ToolbarItem key={'toolbar-new-route'}>
+    <ToolbarItem key="toolbar-new-route">
       <NewFlow />
     </ToolbarItem>,
-    <ToolbarItem key={'toolbar-clipboard'}>
+    <ToolbarItem key="toolbar-clipboard">
       <FlowClipboard />
     </ToolbarItem>,
     <ToolbarItem key={'toolbar-export-image'}>
