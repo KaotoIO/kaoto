c9bab98300973f67b6897aae5c6f7787d2e2fb04
Ricardo M <lordrip@gmail.com>
2023-11-27 20:33:19 +0100

    chore(canvas): Remove Visualization title

--- packages/ui/src/pages/Design/DesignPage.tsx
+++ packages/ui/src/pages/Design/DesignPage.tsx
@@ -1,10 +1,9 @@
-import { Title } from '@patternfly/react-core';
 import { FunctionComponent, useContext } from 'react';
 import { Visualization } from '../../components/Visualization';
 import { CatalogModalProvider } from '../../providers/catalog-modal.provider';
 import { EntitiesContext } from '../../providers/entities.provider';
-import { ReturnToSourceCodeFallback } from './ReturnToSourceCodeFallback';
 import './DesignPage.scss';
+import { ReturnToSourceCodeFallback } from './ReturnToSourceCodeFallback';
 
 export const DesignPage: FunctionComponent = () => {
   const entitiesContext = useContext(EntitiesContext);
@@ -12,8 +11,6 @@ export const DesignPage: FunctionComponent = () => {
 
   return (
     <div className="canvas-page">
-      <Title headingLevel="h1">Visualization</Title>
-
       <CatalogModalProvider>
         <Visualization
           className="canvas-page__canvas"
