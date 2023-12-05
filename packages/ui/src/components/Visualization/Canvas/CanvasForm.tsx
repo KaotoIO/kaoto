45e5400010fa12cc26f46603c2bcaecd55770214
shivamgu <shivamgu@redhat.com>
2023-12-06 03:49:36 +0530

    fix(387): Updated the code

--- packages/ui/src/components/Visualization/Canvas/CanvasForm.tsx
+++ packages/ui/src/components/Visualization/Canvas/CanvasForm.tsx
@@ -18,12 +18,15 @@ export function getNonDefaultProperties(
   obj1: JSONSchemaType<unknown>,
   obj2: Record<string, unknown>,
 ): Record<string, unknown> {
-  const newModelUpdated = Object.entries(obj2.parameters).reduce((acc, currentValue) => {
-    if (!(obj1[currentValue[0]]['default'] == currentValue[1])) {
-      acc.push(currentValue);
-    }
-    return acc;
-  }, []);
+  const newModelUpdated = Object.entries(obj2.parameters as object).reduce(
+    (acc: [string, unknown][], currentValue: [string, unknown]) => {
+      if (!(obj1[currentValue[0]]['default'] == currentValue[1])) {
+        acc.push(currentValue);
+      }
+      return acc;
+    },
+    [],
+  );
   return { ...obj2, parameters: Object.fromEntries(newModelUpdated) };
 }
 
