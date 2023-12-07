be1d97259cc1caa1f6b6261ea7b28a6a85e017e9
tplevko <tplevko@redhat.com>
2023-12-07 12:56:19 +0100

    fix - add route ID format and duplicity validation

---
+++ packages/ui/src/components/InlineEdit/routeIdValidator.test.ts
@@ -0,0 +1,65 @@
+import { ValidationStatus } from '../../models';
+import { CamelRouteResource } from '../../models/camel';
+import { camelRouteJson } from '../../stubs';
+import { RouteIdValidator } from './routeIdValidator';
+
+describe('routeIdValidator', () => {
+  it.each([
+    ['', false],
+    ['.', false],
+    ['/', false],
+    ['Route', false],
+    ['Route 1234', false],
+    ['444', true],
+    ['route', true],
+    ['route-1234', true],
+  ])('isNameValidCheck() should return "%s" when name="%s"', (name, result) => {
+    expect(RouteIdValidator.isNameValidCheck(name)).toEqual(result);
+  });
+
+  it('should return sucess if the name is unique', () => {
+    const resource = new CamelRouteResource(camelRouteJson);
+    const visualEntities = resource.getVisualEntities();
+    jest.spyOn(visualEntities[0], 'getId').mockReturnValue('flow-1234');
+
+    const result = RouteIdValidator.validateUniqueName('unique-name', visualEntities);
+    expect(result.status).toEqual(ValidationStatus.Success);
+    expect(result.errMessages).toHaveLength(0);
+  });
+
+  it('should return an error if the name is not unique', () => {
+    const resource = new CamelRouteResource(camelRouteJson);
+    const visualEntities = resource.getVisualEntities();
+    jest.spyOn(visualEntities[0], 'getId').mockReturnValue('flow-1234');
+
+    const result = RouteIdValidator.validateUniqueName('flow-1234', visualEntities);
+
+    expect(result.status).toEqual(ValidationStatus.Error);
+    expect(result.errMessages).toEqual(['Name must be unique']);
+  });
+
+  it('should return an error if the name is not a valid URI', () => {
+    const resource = new CamelRouteResource(camelRouteJson);
+    const visualEntities = resource.getVisualEntities();
+    jest.spyOn(visualEntities[0], 'getId').mockReturnValue('flow-1234');
+
+    const result = RouteIdValidator.validateUniqueName('The amazing Route', visualEntities);
+
+    expect(result.status).toEqual(ValidationStatus.Error);
+    expect(result.errMessages).toEqual(['Name should only contain lowercase letters, numbers, and dashes']);
+  });
+
+  it('should return an error if the name is not unique neither a valid URI', () => {
+    const resource = new CamelRouteResource(camelRouteJson);
+    const visualEntities = resource.getVisualEntities();
+    jest.spyOn(visualEntities[0], 'getId').mockReturnValue('The amazing Route');
+
+    const result = RouteIdValidator.validateUniqueName('The amazing Route', visualEntities);
+
+    expect(result.status).toEqual(ValidationStatus.Error);
+    expect(result.errMessages).toEqual([
+      'Name should only contain lowercase letters, numbers, and dashes',
+      'Name must be unique',
+    ]);
+  });
+});
---
+++ packages/ui/src/components/InlineEdit/routeIdValidator.ts
@@ -0,0 +1,38 @@
+import { ValidationResult, ValidationStatus } from '../../models';
+import { BaseVisualCamelEntity } from '../../models/visualization/base-visual-entity';
+
+export class RouteIdValidator {
+  private static URI_REGEXP = /^[a-z\d]([-a-z\d]*[a-z\d])?(\.[a-z\d]([-a-z\d]*[a-z\d])?)*$/gm;
+
+  /**
+   * Verifies that the provided name is valid
+   * Regex: [a-z0-9]([-a-z0-9]*[a-z0-9])?(\.[a-z0-9]([-a-z0-9]*[a-z0-9])?)*
+   * @param name
+   */
+  static isNameValidCheck(name: string): boolean {
+    const isValid = RouteIdValidator.URI_REGEXP.test(name);
+    RouteIdValidator.URI_REGEXP.lastIndex = 0;
+
+    return isValid;
+  }
+
+  static validateUniqueName(flowName: string, visualEntities: BaseVisualCamelEntity[]): ValidationResult {
+    const errMessages = [];
+    const flowsIds = visualEntities.map((flow) => flow.getId());
+
+    const isValidURI = RouteIdValidator.isNameValidCheck(flowName);
+    if (!isValidURI) {
+      errMessages.push('Name should only contain lowercase letters, numbers, and dashes');
+    }
+
+    const isUnique = !flowsIds.includes(flowName);
+    if (!isUnique) {
+      errMessages.push('Name must be unique');
+    }
+
+    return {
+      status: isValidURI && isUnique ? ValidationStatus.Success : ValidationStatus.Error,
+      errMessages,
+    };
+  }
+}
