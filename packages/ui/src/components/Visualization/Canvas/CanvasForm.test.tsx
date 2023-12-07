3106e8e51e97d5bc1cf62892a423512ea3f51743
shivamgu <shivamgu@redhat.com>
2023-12-07 20:47:19 +0530

    fix(387): Handled empty inputs

--- packages/ui/src/components/Visualization/Canvas/CanvasForm.test.tsx
+++ packages/ui/src/components/Visualization/Canvas/CanvasForm.test.tsx
@@ -10,7 +10,7 @@ import { CustomAutoField } from '../../Form/CustomAutoField';
 import * as componentCatalogMap from '@kaoto-next/camel-catalog/camel-catalog-aggregate-components.json';
 import * as kameletCatalogMap from '@kaoto-next/camel-catalog/kamelets-aggregate.json';
 import { SchemaService } from '../../Form';
-import { getNonDefaultProperties } from './CanvasForm';
+import { getNonDefaultProperties, getNonEmptyProperties } from './CanvasForm';
 
 describe('CanvasForm', () => {
   const omitFields = ['expression', 'dataFormatType', 'outputs', 'steps', 'when', 'otherwise', 'doCatch', 'doFinally'];
@@ -216,6 +216,82 @@ describe('CanvasForm getNonDefaultProperties()', () => {
 
   it('should return only the properties which are different from default', () => {
     const newModelClean = getNonDefaultProperties(schema?.properties.parameters.properties, newModel);
+    expect(newModelClean).toMatchObject(newModelExpected);
+  });
+});
+
+describe('CanvasForm getNonEmptyProperties()', () => {
+  const schema = {
+    type: 'object',
+    properties: {
+      parameters: {
+        properties: {
+          events: {
+            type: 'string',
+            default: 'CREATE,MODIFY,DELETE',
+            title: 'Events',
+          },
+          concurrentConsumers: {
+            type: 'integer',
+            default: 1,
+            title: 'Concurrent Consumers',
+          },
+          bridgeErrorHandler: {
+            type: 'boolean',
+            default: false,
+            title: 'Bridge Error Handler',
+          },
+          exchangePattern: {
+            type: 'object',
+            title: 'Exchange Pattern',
+          },
+        },
+      },
+    },
+  } as unknown as JSONSchemaType<unknown>;
+
+  const newModel: Record<string, unknown> = {
+    id: 'from-7126',
+    description: 'test',
+    steps: [],
+    uri: 'file-watch',
+    parameters: {
+      events: 'CREATE',
+      concurrentConsumers: '',
+      bridgeErrorHandler: false,
+      exchangePattern: {},
+    },
+  };
+
+  const newModelIntermediate: Record<string, unknown> = {
+    id: 'from-7126',
+    description: 'test',
+    steps: [],
+    uri: 'file-watch',
+    parameters: {
+      events: 'CREATE',
+      concurrentConsumers: '',
+      exchangePattern: {},
+    },
+  };
+
+  const newModelExpected: Record<string, unknown> = {
+    id: 'from-7126',
+    description: 'test',
+    steps: [],
+    uri: 'file-watch',
+    parameters: {
+      events: 'CREATE',
+    },
+  };
+
+  it('should return only the properties which are different from default', () => {
+    const newModelClean = getNonDefaultProperties(schema?.properties.parameters.properties, newModel);
+    expect(newModelClean).toMatchObject(newModelIntermediate);
+  });
+
+  it('should return only the non-empty properties', () => {
+    const newModelClean = getNonEmptyProperties(newModel);
     expect(newModelClean).toMatchObject(newModelExpected);
   });
 });
