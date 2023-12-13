26cd4e1d9d37bf81661847c7584809c9f73abc0b
Tomohisa Igarashi <tm.igarashi@gmail.com>
2023-12-13 09:11:10 -0500

    fix: Create BeansEntity if not exist (#470)
    Otherwise it fails to add a bean if there's no existing `beans`

--- packages/ui/src/components/Form/bean/BeanReferenceField.test.tsx
+++ packages/ui/src/components/Form/bean/BeanReferenceField.test.tsx
@@ -10,32 +10,11 @@ import { useSchemasStore } from '../../../store';
 import { CustomAutoFieldDetector } from '../CustomAutoField';
 import { SchemaService } from '../schema.service';
 import { BeanReferenceField } from './BeanReferenceField';
+import { BeansAwareResource, CamelRouteResource } from '../../../models/camel';
 
 useSchemasStore.setState({
   schemas: { beans: { name: 'beans', tags: [], version: '0', uri: '', schema: beansSchema } },
 });
-
-const beans = {
-  beans: [
-    {
-      name: 'myDataSource',
-      type: 'org.apache.commons.dbcp.BasicDataSource',
-    },
-    {
-      name: 'myBean',
-      type: 'io.kaoto.MyBean',
-    },
-  ],
-};
-
-const beansEntity = new BeansEntity(beans);
-const contextValue = {
-  camelResource: {
-    getEntities: () => {
-      return [beansEntity];
-    },
-  },
-} as unknown as EntitiesContextResult;
 
 describe('BeanReferenceField', () => {
   const mockSchema = {
@@ -68,10 +47,40 @@ describe('BeanReferenceField', () => {
     mockOnChange.mockClear();
   });
 
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  function doGetContextValue(beans?: any) {
+    const camelResource = new CamelRouteResource();
+    if (beans) {
+      const beansEntity = (camelResource as BeansAwareResource).createBeansEntity();
+      beansEntity.parent = beans;
+    }
+    return { camelResource: camelResource } as unknown as EntitiesContextResult;
+  }
+
+  function getContextValue() {
+    const beans = {
+      beans: [
+        {
+          name: 'myDataSource',
+          type: 'org.apache.commons.dbcp.BasicDataSource',
+        },
+        {
+          name: 'myBean',
+          type: 'io.kaoto.MyBean',
+        },
+      ],
+    };
+    return doGetContextValue(beans);
+  }
+
+  function getContextWithoutBeans() {
+    return doGetContextValue();
+  }
+
   it('should render', async () => {
     await act(async () => {
       render(
-        <EntitiesContext.Provider value={contextValue}>
+        <EntitiesContext.Provider value={getContextValue()}>
           <AutoField.componentDetectorContext.Provider value={CustomAutoFieldDetector}>
             <AutoForm schema={schemaBridge} model={{}}>
               <BeanReferenceField name="beanName" label="Bean reference field label" {...fieldProperties} />
@@ -98,7 +107,7 @@ describe('BeanReferenceField', () => {
   it('should update model if the other is selected', async () => {
     await act(async () => {
       render(
-        <EntitiesContext.Provider value={contextValue}>
+        <EntitiesContext.Provider value={getContextValue()}>
           <AutoField.componentDetectorContext.Provider value={CustomAutoFieldDetector}>
             <AutoForm schema={schemaBridge} model={{}}>
               <BeanReferenceField name="beanName" label="Bean reference field label" {...fieldProperties} />
@@ -128,7 +137,7 @@ describe('BeanReferenceField', () => {
   it("should update model even if there's no bean", async () => {
     await act(async () => {
       render(
-        <EntitiesContext.Provider value={contextValue}>
+        <EntitiesContext.Provider value={getContextValue()}>
           <AutoField.componentDetectorContext.Provider value={CustomAutoFieldDetector}>
             <AutoForm schema={schemaBridge} model={{}}>
               <BeanReferenceField name="beanName" label="Bean reference field label" {...fieldProperties} />
@@ -152,7 +161,7 @@ describe('BeanReferenceField', () => {
   it('should filter options when typing', async () => {
     await act(async () => {
       render(
-        <EntitiesContext.Provider value={contextValue}>
+        <EntitiesContext.Provider value={getContextValue()}>
           <AutoField.componentDetectorContext.Provider value={CustomAutoFieldDetector}>
             <AutoForm schema={schemaBridge} model={{}}>
               <BeanReferenceField name="beanName" label="Bean reference field label" {...fieldProperties} />
@@ -177,7 +186,7 @@ describe('BeanReferenceField', () => {
   it('should render a modal', async () => {
     await act(async () => {
       render(
-        <EntitiesContext.Provider value={contextValue}>
+        <EntitiesContext.Provider value={getContextValue()}>
           <AutoField.componentDetectorContext.Provider value={CustomAutoFieldDetector}>
             <AutoForm schema={schemaBridge} model={{}}>
               <BeanReferenceField name="beanName" label="Bean reference field label" {...fieldProperties} />
@@ -203,5 +212,103 @@ describe('BeanReferenceField', () => {
       const modal = screen.getByRole('dialog');
       expect(modal).toBeInTheDocument();
     });
+  });
+
+  it('should render a modal with bean name, then creating one fills the reference', async () => {
+    const contextValue = getContextValue();
+    await act(async () => {
+      render(
+        <EntitiesContext.Provider value={contextValue}>
+          <AutoField.componentDetectorContext.Provider value={CustomAutoFieldDetector}>
+            <AutoForm schema={schemaBridge} model={{}}>
+              <BeanReferenceField name="beanName" label="Bean reference field label" {...fieldProperties} />
+            </AutoForm>
+          </AutoField.componentDetectorContext.Provider>
+        </EntitiesContext.Provider>,
+      );
+    });
+
+    const textbox = screen.getByRole('combobox');
+    await act(async () => {
+      fireEvent.input(textbox, { target: { value: 'myNewBean' } });
+    });
+
+    const createNewOption = screen
+      .getAllByRole('option')
+      .filter((option) => option.textContent?.includes('Create new bean'));
+    expect(createNewOption).toHaveLength(1);
+    await act(async () => {
+      fireEvent.click(createNewOption[0]);
+    });
+
+    const nameInput = screen.getAllByRole('textbox').filter((i) => i.getAttribute('label') === 'Name');
+    expect(nameInput).toHaveLength(1);
+    expect(nameInput[0]).toHaveValue('myNewBean');
+    const typeInput = screen.getAllByRole('textbox').filter((i) => i.getAttribute('label') === 'Type');
+    expect(typeInput).toHaveLength(1);
+    await act(async () => {
+      fireEvent.input(typeInput[0], { target: { value: 'io.kaoto.new.MyNewBean' } });
+    });
+
+    const createButton = screen.getAllByRole('button').filter((b) => b.textContent === 'Create');
+    expect(createButton).toHaveLength(1);
+    await act(async () => {
+      fireEvent.click(createButton[0]);
+    });
+
+    expect(screen.queryByRole('dialog')).toBeNull();
+    const textbox2 = screen.getByRole('combobox');
+    expect(textbox2.getAttribute('value')).toEqual('#myNewBean');
+    const entity = contextValue.camelResource.getEntities()[0] as BeansEntity;
+    expect(Object.keys(entity.parent.beans).length).toBe(3);
+  });
+
+  it("should render a modal with bean name, then creating one fills the reference even if beans entity doesn't exist", async () => {
+    const contextValue = getContextWithoutBeans();
+    await act(async () => {
+      render(
+        <EntitiesContext.Provider value={contextValue}>
+          <AutoField.componentDetectorContext.Provider value={CustomAutoFieldDetector}>
+            <AutoForm schema={schemaBridge} model={{}}>
+              <BeanReferenceField name="beanName" label="Bean reference field label" {...fieldProperties} />
+            </AutoForm>
+          </AutoField.componentDetectorContext.Provider>
+        </EntitiesContext.Provider>,
+      );
+    });
+
+    const textbox = screen.getByRole('combobox');
+    await act(async () => {
+      fireEvent.input(textbox, { target: { value: 'myNewBean' } });
+    });
+
+    const createNewOption = screen
+      .getAllByRole('option')
+      .filter((option) => option.textContent?.includes('Create new bean'));
+    expect(createNewOption).toHaveLength(1);
+    await act(async () => {
+      fireEvent.click(createNewOption[0]);
+    });
+
+    const nameInput = screen.getAllByRole('textbox').filter((i) => i.getAttribute('label') === 'Name');
+    expect(nameInput).toHaveLength(1);
+    expect(nameInput[0]).toHaveValue('myNewBean');
+    const typeInput = screen.getAllByRole('textbox').filter((i) => i.getAttribute('label') === 'Type');
+    expect(typeInput).toHaveLength(1);
+    await act(async () => {
+      fireEvent.input(typeInput[0], { target: { value: 'io.kaoto.new.MyNewBean' } });
+    });
+
+    const createButton = screen.getAllByRole('button').filter((b) => b.textContent === 'Create');
+    expect(createButton).toHaveLength(1);
+    await act(async () => {
+      fireEvent.click(createButton[0]);
+    });
+
+    expect(screen.queryByRole('dialog')).toBeNull();
+    const textbox2 = screen.getByRole('combobox');
+    expect(textbox2.getAttribute('value')).toEqual('#myNewBean');
+    const entity = contextValue.camelResource.getEntities()[0] as BeansEntity;
+    expect(Object.keys(entity.parent.beans).length).toBe(1);
   });
 });
