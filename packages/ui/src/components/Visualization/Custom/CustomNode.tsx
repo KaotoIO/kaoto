c055910691f8feaa2c865c4cba964ad09a736bff
shivamgu <shivamgu@redhat.com>
2024-01-12 19:30:15 +0530

    fix(538): initial commit

--- packages/ui/src/components/Visualization/Custom/CustomNode.tsx
+++ packages/ui/src/components/Visualization/Custom/CustomNode.tsx
@@ -1,4 +1,5 @@
 import { DefaultNode, Node, WithSelectionProps, withContextMenu, withSelection } from '@patternfly/react-topology';
+import { Tooltip } from '@patternfly/react-core';
 import { FunctionComponent } from 'react';
 import { AddStepMode } from '../../../models/visualization/base-visual-entity';
 import { CanvasDefaults } from '../Canvas/canvas.defaults';
@@ -17,6 +18,11 @@ const CustomNode: FunctionComponent<CustomNodeProps> = ({ element, ...rest }) =>
   const vizNode = element.getData()?.vizNode;
   const label = vizNode?.getNodeLabel();
 
+  let tooltipContent = vizNode?.getComponentSchema()?.definition.description;
+  if (tooltipContent === undefined || tooltipContent === null) {
+    tooltipContent = vizNode?.getComponentSchema()?.schema.description;
+  }
+
   return (
     <DefaultNode element={element} label={label} truncateLength={15} {...rest} showStatusDecorator>
       <g data-testid={`custom-node__${vizNode?.id}`} data-nodelabel={label}>
@@ -26,13 +32,15 @@ const CustomNode: FunctionComponent<CustomNodeProps> = ({ element, ...rest }) =>
           width={CanvasDefaults.DEFAULT_NODE_DIAMETER}
           height={CanvasDefaults.DEFAULT_NODE_DIAMETER}
         >
-          <div className="custom-node__image">
-            <img
-              src={vizNode?.data.icon}
-              width={CanvasDefaults.DEFAULT_NODE_DIAMETER * 0.7}
-              height={CanvasDefaults.DEFAULT_NODE_DIAMETER * 0.7}
-            />
-          </div>
+          <Tooltip content={tooltipContent}>
+            <div className="custom-node__image">
+              <img
+                src={vizNode?.data.icon}
+                width={CanvasDefaults.DEFAULT_NODE_DIAMETER * 0.7}
+                height={CanvasDefaults.DEFAULT_NODE_DIAMETER * 0.7}
+              />
+            </div>
+          </Tooltip>
         </foreignObject>
       </g>
     </DefaultNode>
