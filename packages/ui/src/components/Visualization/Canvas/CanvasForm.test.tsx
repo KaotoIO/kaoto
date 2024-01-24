5b73eb8ccdd7f37a7a24070c176ceb3fe4761c90
Tomohisa Igarashi <tm.igarashi@gmail.com>
2024-01-24 12:41:57 -0500

    chore: Add Jest tests for #722

--- packages/ui/src/components/Visualization/Canvas/CanvasForm.test.tsx
+++ packages/ui/src/components/Visualization/Canvas/CanvasForm.test.tsx
@@ -1,6 +1,6 @@
 import * as catalogIndex from '@kaoto-next/camel-catalog/index.json';
 import { AutoField, AutoFields } from '@kaoto-next/uniforms-patternfly';
-import { render } from '@testing-library/react';
+import { fireEvent, render, screen } from '@testing-library/react';
 import { JSONSchemaType } from 'ajv';
 import { AutoForm } from 'uniforms';
 import { IVisualizationNode, VisualComponentSchema } from '../../../models/visualization/base-visual-entity';
@@ -9,6 +9,17 @@ import { SchemaService } from '../../Form';
 import { CustomAutoFieldDetector } from '../../Form/CustomAutoField';
 import { CanvasForm } from './CanvasForm';
 import { CanvasNode } from './canvas.models';
+import {
+  CamelCatalogService,
+  CamelRouteVisualEntity,
+  CatalogKind,
+  ICamelDataformatDefinition,
+  ICamelLanguageDefinition,
+  ICamelLoadBalancerDefinition,
+  ICamelProcessorDefinition,
+} from '../../../models';
+import { act } from 'react-dom/test-utils';
+import { RouteDefinition } from '@kaoto-next/camel-catalog/types';
 
 describe('CanvasForm', () => {
   const omitFields = ['expression', 'dataFormatType', 'outputs', 'steps', 'when', 'otherwise', 'doCatch', 'doFinally'];
@@ -22,6 +33,33 @@ describe('CanvasForm', () => {
       },
     },
   } as unknown as JSONSchemaType<unknown>;
+
+  beforeAll(async () => {
+    const patternCatalog = await import('@kaoto-next/camel-catalog/' + catalogIndex.catalogs.patterns.file);
+    delete patternCatalog.default;
+    const languageCatalog = await import('@kaoto-next/camel-catalog/' + catalogIndex.catalogs.languages.file);
+    delete languageCatalog.default;
+    const dataformatCatalog = await import('@kaoto-next/camel-catalog/' + catalogIndex.catalogs.dataformats.file);
+    delete dataformatCatalog.default;
+    const loadbalancerCatalog = await import('@kaoto-next/camel-catalog/' + catalogIndex.catalogs.loadbalancers.file);
+    delete loadbalancerCatalog.default;
+    CamelCatalogService.setCatalogKey(
+      CatalogKind.Pattern,
+      patternCatalog as unknown as Record<string, ICamelProcessorDefinition>,
+    );
+    CamelCatalogService.setCatalogKey(
+      CatalogKind.Language,
+      languageCatalog as unknown as Record<string, ICamelLanguageDefinition>,
+    );
+    CamelCatalogService.setCatalogKey(
+      CatalogKind.Dataformat,
+      dataformatCatalog as unknown as Record<string, ICamelDataformatDefinition>,
+    );
+    CamelCatalogService.setCatalogKey(
+      CatalogKind.Loadbalancer,
+      loadbalancerCatalog as unknown as Record<string, ICamelLoadBalancerDefinition>,
+    );
+  });
 
   it('should render', () => {
     const visualComponentSchema: VisualComponentSchema = {
@@ -124,6 +162,307 @@ describe('CanvasForm', () => {
 
     expect(visualComponentSchema.definition.parameters).toEqual({});
   });
+
+  describe('should persists changes from both expression editor and main form', () => {
+    it('expression => main form', async () => {
+      const camelRoute = {
+        from: {
+          uri: 'timer:tutorial',
+          steps: [
+            {
+              setHeader: {
+                name: 'foo',
+              },
+            },
+          ],
+        },
+      } as RouteDefinition;
+      const entity = new CamelRouteVisualEntity(camelRoute);
+      const rootNode: IVisualizationNode = entity.toVizNode();
+      const selectedNode = {
+        id: '1',
+        type: 'node',
+        data: {
+          vizNode: rootNode.getChildren()![0],
+        },
+      };
+
+      render(
+        <EntitiesContext.Provider value={null}>
+          <CanvasForm selectedNode={selectedNode as unknown as CanvasNode} />
+        </EntitiesContext.Provider>,
+      );
+      const launchExpression = screen.getByTestId('launch-expression-modal-btn');
+      act(() => {
+        fireEvent.click(launchExpression);
+      });
+      const expressionInput = screen
+        .getAllByRole('textbox')
+        .filter((textbox) => textbox.getAttribute('label') === 'Expression');
+      const applyBtn = screen.getAllByRole('button').filter((button) => button.textContent === 'Apply');
+      act(() => {
+        fireEvent.input(expressionInput[0], { target: { value: '${header.foo}' } });
+        fireEvent.click(applyBtn[0]);
+      });
+      /* eslint-disable  @typescript-eslint/no-explicit-any */
+      expect((camelRoute.from.steps[0].setHeader!.expression as any).simple.expression).toEqual('${header.foo}');
+      expect(camelRoute.from.steps[0].setHeader!.name).toEqual('foo');
+
+      const filtered = screen.getAllByRole('textbox').filter((textbox) => textbox.getAttribute('label') === 'Name');
+      act(() => {
+        fireEvent.input(filtered[0], { target: { value: 'bar' } });
+      });
+      /* eslint-disable  @typescript-eslint/no-explicit-any */
+      expect((camelRoute.from.steps[0].setHeader!.expression as any).simple.expression).toEqual('${header.foo}');
+      expect(camelRoute.from.steps[0].setHeader!.name).toEqual('bar');
+    });
+
+    it('main form => expression', async () => {
+      const camelRoute = {
+        from: {
+          uri: 'timer:tutorial',
+          steps: [
+            {
+              setHeader: {
+                name: 'foo',
+              },
+            },
+          ],
+        },
+      } as RouteDefinition;
+      const entity = new CamelRouteVisualEntity(camelRoute);
+      const rootNode: IVisualizationNode = entity.toVizNode();
+      const selectedNode = {
+        id: '1',
+        type: 'node',
+        data: {
+          vizNode: rootNode.getChildren()![0],
+        },
+      };
+
+      render(
+        <EntitiesContext.Provider value={null}>
+          <CanvasForm selectedNode={selectedNode as unknown as CanvasNode} />
+        </EntitiesContext.Provider>,
+      );
+      const filtered = screen.getAllByRole('textbox').filter((textbox) => textbox.getAttribute('label') === 'Name');
+      act(() => {
+        fireEvent.input(filtered[0], { target: { value: 'bar' } });
+      });
+      expect(camelRoute.from.steps[0].setHeader!.expression).toBeUndefined();
+      expect(camelRoute.from.steps[0].setHeader!.name).toEqual('bar');
+
+      const launchExpression = screen.getByTestId('launch-expression-modal-btn');
+      act(() => {
+        fireEvent.click(launchExpression);
+      });
+      const expressionInput = screen
+        .getAllByRole('textbox')
+        .filter((textbox) => textbox.getAttribute('label') === 'Expression');
+      const applyBtn = screen.getAllByRole('button').filter((button) => button.textContent === 'Apply');
+      act(() => {
+        fireEvent.input(expressionInput[0], { target: { value: '${header.foo}' } });
+        fireEvent.click(applyBtn[0]);
+      });
+      /* eslint-disable  @typescript-eslint/no-explicit-any */
+      expect((camelRoute.from.steps[0].setHeader!.expression as any).simple.expression).toEqual('${header.foo}');
+      expect(camelRoute.from.steps[0].setHeader!.name).toEqual('bar');
+    });
+  });
+
+  describe('should persists changes from both dataformat editor and main form', () => {
+    it('dataformat => main form', async () => {
+      const camelRoute = {
+        from: {
+          uri: 'timer:tutorial',
+          steps: [
+            {
+              marshal: {
+                id: 'ms',
+              },
+            },
+          ],
+        },
+      } as RouteDefinition;
+      const entity = new CamelRouteVisualEntity(camelRoute);
+      const rootNode: IVisualizationNode = entity.toVizNode();
+      const selectedNode = {
+        id: '1',
+        type: 'node',
+        data: {
+          vizNode: rootNode.getChildren()![0],
+        },
+      };
+
+      render(
+        <EntitiesContext.Provider value={null}>
+          <CanvasForm selectedNode={selectedNode as unknown as CanvasNode} />
+        </EntitiesContext.Provider>,
+      );
+      const button = screen.getAllByRole('button').filter((button) => button.innerHTML.includes('JSon'));
+      act(() => {
+        fireEvent.click(button[0]);
+      });
+      const avro = screen.getByTestId('dataformat-dropdownitem-avro');
+      act(() => {
+        fireEvent.click(avro.getElementsByTagName('button')[0]);
+      });
+      expect(camelRoute.from.steps[0].marshal!.avro).toBeDefined();
+      expect(camelRoute.from.steps[0].marshal!.id).toEqual('ms');
+
+      const idInput = screen.getAllByRole('textbox').filter((textbox) => textbox.getAttribute('label') === 'Id');
+      act(() => {
+        fireEvent.input(idInput[1], { target: { value: 'modified' } });
+      });
+      expect(camelRoute.from.steps[0].marshal!.avro).toBeDefined();
+      expect(camelRoute.from.steps[0].marshal!.id).toEqual('modified');
+    });
+    it('main form => dataformat', async () => {
+      const camelRoute = {
+        from: {
+          uri: 'timer:tutorial',
+          steps: [
+            {
+              marshal: {
+                id: 'ms',
+              },
+            },
+          ],
+        },
+      } as RouteDefinition;
+      const entity = new CamelRouteVisualEntity(camelRoute);
+      const rootNode: IVisualizationNode = entity.toVizNode();
+      const selectedNode = {
+        id: '1',
+        type: 'node',
+        data: {
+          vizNode: rootNode.getChildren()![0],
+        },
+      };
+
+      render(
+        <EntitiesContext.Provider value={null}>
+          <CanvasForm selectedNode={selectedNode as unknown as CanvasNode} />
+        </EntitiesContext.Provider>,
+      );
+      const idInput = screen.getAllByRole('textbox').filter((textbox) => textbox.getAttribute('label') === 'Id');
+      act(() => {
+        fireEvent.input(idInput[1], { target: { value: 'modified' } });
+      });
+      expect(camelRoute.from.steps[0].marshal!.avro).toBeUndefined();
+      expect(camelRoute.from.steps[0].marshal!.id).toEqual('modified');
+
+      const button = screen.getAllByRole('button').filter((button) => button.innerHTML.includes('JSon'));
+      act(() => {
+        fireEvent.click(button[0]);
+      });
+      const avro = screen.getByTestId('dataformat-dropdownitem-avro');
+      act(() => {
+        fireEvent.click(avro.getElementsByTagName('button')[0]);
+      });
+      expect(camelRoute.from.steps[0].marshal!.avro).toBeDefined();
+      expect(camelRoute.from.steps[0].marshal!.id).toEqual('modified');
+    });
+  });
+
+  describe('should persists changes from both loadbalancer editor and main form', () => {
+    it('loadbalancer => main form', async () => {
+      const camelRoute = {
+        from: {
+          uri: 'timer:tutorial',
+          steps: [
+            {
+              loadBalance: {
+                id: 'lb',
+              },
+            },
+          ],
+        },
+      } as RouteDefinition;
+      const entity = new CamelRouteVisualEntity(camelRoute);
+      const rootNode: IVisualizationNode = entity.toVizNode();
+      const selectedNode = {
+        id: '1',
+        type: 'node',
+        data: {
+          vizNode: rootNode.getChildren()![0],
+        },
+      };
+
+      render(
+        <EntitiesContext.Provider value={null}>
+          <CanvasForm selectedNode={selectedNode as unknown as CanvasNode} />
+        </EntitiesContext.Provider>,
+      );
+      const button = screen.getAllByRole('button').filter((button) => button.innerHTML.includes('Round Robin'));
+      act(() => {
+        fireEvent.click(button[0]);
+      });
+      const avro = screen.getByTestId('loadbalancer-dropdownitem-weighted');
+      act(() => {
+        fireEvent.click(avro.getElementsByTagName('button')[0]);
+      });
+      expect(camelRoute.from.steps[0].loadBalance!.weighted).toBeDefined();
+      expect(camelRoute.from.steps[0].loadBalance!.id).toEqual('lb');
+
+      const idInput = screen.getAllByRole('textbox').filter((textbox) => textbox.getAttribute('label') === 'Id');
+      act(() => {
+        fireEvent.input(idInput[1], { target: { value: 'modified' } });
+      });
+      expect(camelRoute.from.steps[0].loadBalance!.weighted).toBeDefined();
+      expect(camelRoute.from.steps[0].loadBalance!.id).toEqual('modified');
+    });
+    it('main form => loadbalancer', async () => {
+      const camelRoute = {
+        from: {
+          uri: 'timer:tutorial',
+          steps: [
+            {
+              loadBalance: {
+                id: 'lb',
+              },
+            },
+          ],
+        },
+      } as RouteDefinition;
+      const entity = new CamelRouteVisualEntity(camelRoute);
+      const rootNode: IVisualizationNode = entity.toVizNode();
+      const selectedNode = {
+        id: '1',
+        type: 'node',
+        data: {
+          vizNode: rootNode.getChildren()![0],
+        },
+      };
+
+      render(
+        <EntitiesContext.Provider value={null}>
+          <CanvasForm selectedNode={selectedNode as unknown as CanvasNode} />
+        </EntitiesContext.Provider>,
+      );
+      const idInput = screen.getAllByRole('textbox').filter((textbox) => textbox.getAttribute('label') === 'Id');
+      act(() => {
+        fireEvent.input(idInput[1], { target: { value: 'modified' } });
+      });
+      expect(camelRoute.from.steps[0].loadBalance!.weighted).toBeUndefined();
+      expect(camelRoute.from.steps[0].loadBalance!.id).toEqual('modified');
+
+      const button = screen.getAllByRole('button').filter((button) => button.innerHTML.includes('Round Robin'));
+      act(() => {
+        fireEvent.click(button[0]);
+      });
+      const avro = screen.getByTestId('loadbalancer-dropdownitem-weighted');
+      act(() => {
+        fireEvent.click(avro.getElementsByTagName('button')[0]);
+      });
+      expect(camelRoute.from.steps[0].loadBalance!.weighted).toBeDefined();
+      expect(camelRoute.from.steps[0].loadBalance!.id).toEqual('modified');
+    });
+  });
+
+  /*
+   * Exhaustive tests
+   */
 
   it('should render for all component without an error', async () => {
     const componentCatalogMap = await import('@kaoto-next/camel-catalog/' + catalogIndex.catalogs.components.file);
