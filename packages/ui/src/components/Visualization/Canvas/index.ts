79fdb7e8d6d9e53a31e3915deb8c6cdc04d36f91
shivamgu <shivamgu@redhat.com>
2024-01-25 11:32:24 +0530

    fix(647): Fix commit

--- packages/ui/src/components/Visualization/Canvas/index.ts
+++ packages/ui/src/components/Visualization/Canvas/index.ts
@@ -1,5 +1,3 @@
 export * from './Canvas';
 export * from './CanvasSideBar';
-export * from '../../Form/dataFormat/DataFormatEditor';
-export * from '../../Form/stepExpression/StepExpressionEditor';
 export * from './canvas.models';
