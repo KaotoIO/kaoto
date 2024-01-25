861ce1236d200f73272a672713e15bc0da3bad3d
tplevko <tplevko@redhat.com>
2024-01-25 16:19:00 +0100

    fix(677) enable users to enter text into drop down boxes

--- packages/ui/src/components/Form/customField/CustomSelectField.tsx
+++
@@ -1,6 +0,0 @@
-import { SelectField, SelectFieldProps } from '@kaoto-next/uniforms-patternfly';
-import { FunctionComponent } from 'react';
-
-export const CustomSelectField: FunctionComponent<SelectFieldProps> = (props) => {
-  return <SelectField {...(props as SelectFieldProps)} placeholder="Please select an option..." />;
-};
