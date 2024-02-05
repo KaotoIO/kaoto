2e7b836808f5a3fa125a28ca1f6397c25d799a9c
Ricardo M <lordrip@gmail.com>
2024-02-05 21:41:24 +0100

    chore(catalog): Add description to `CatalogKind`
    This commit adds a brief description to the `CatalogKind` enum to
    understand what's available on each Catalog kind.
    
    relates to: https://github.com/KaotoIO/kaoto-next/issues/767

--- packages/ui/src/models/catalog-kind.ts
+++ packages/ui/src/models/catalog-kind.ts
@@ -1,10 +1,26 @@
+/** Kaoto catalog kinds */
 export const enum CatalogKind {
+  /** Camel components catalog, f.i. amqp, log, timer */
   Component = 'component',
+
+  /** Camel model catalog, f.i. route, from, eips, routeTemplate, languages, dataformats, loadbalancer */
   Processor = 'processor',
+
+  /** Camel processors (EIPs) definitions, f.i. log, to, toD, transform, filter */
   Pattern = 'pattern',
+
+  /** Camel entities catalog, f.i. from, route, routeTemplate */
   Entity = 'entity',
+
+  /** Camel languages catalog, f.i. simple, groovy, kotlin */
   Language = 'language',
+
+  /** Camel dataformats catalog, f.i. json, xml, csv */
   Dataformat = 'dataformat',
+
+  /** Camel loadbalancer catalog, f.i. round robin, failover, random */
   Loadbalancer = 'loadbalancer',
+
+  /** Camel kamelets catalog, f.i. xj-template-action */
   Kamelet = 'kamelet',
 }
