6cec8e522fab43723329b618641f59b01ff6d72a
Ricardo M <lordrip@gmail.com>
2024-02-12 14:59:51 +0100

    feat(canvas-form): Use CustomAutoForm
    Currently, there are 2 different `AutoForm` instances, one from `CanvasForm`
    and another one from the `MetadataEditor`.
    
    This leads to code duplication and different form behavior depending on
    which screen is displayed.
    
    This commit updates the `CanvasForm` and `MetadataEditor` components to
    use the `CustomAutoForm`.

--- packages/ui/src/components/Form/CustomAutoForm.tsx
+++ packages/ui/src/components/Form/CustomAutoForm.tsx
@@ -1,4 +1,4 @@
-import { AutoField, AutoForm, ErrorsField } from '@kaoto-next/uniforms-patternfly';
+import { AutoField, AutoFields, AutoForm, ErrorsField } from '@kaoto-next/uniforms-patternfly';
 import { forwardRef, useImperativeHandle, useMemo, useRef } from 'react';
 import { JSONSchemaBridge } from 'uniforms-bridge-json-schema';
 import { IDataTestID } from '../../models';
@@ -8,18 +8,29 @@ interface CustomAutoFormProps extends IDataTestID {
   schemaBridge?: JSONSchemaBridge;
   model: unknown;
   disabled?: boolean;
-  onChangeModel: (model: unknown) => void;
+  sortFields?: boolean;
+  omitFields?: string[];
+  onChangeModel?: (model: unknown) => void;
+  onChange?: (path: string, value: unknown) => void;
 }
 
-export const CustomAutoForm = forwardRef<{ fields: HTMLElement[] }, CustomAutoFormProps>((props, forwardedRef) => {
+export type CustomAutoFormRef = { fields: HTMLElement[]; form: typeof AutoForm };
+
+export const CustomAutoForm = forwardRef<CustomAutoFormRef, CustomAutoFormProps>((props, forwardedRef) => {
+  const formRef = useRef<typeof AutoForm>();
   const fieldsRefs = useRef<HTMLElement[]>([]);
   const sortedFieldsNames = useMemo(() => {
-    if (props.schemaBridge === undefined) {
+    if (props.schemaBridge === undefined || !props.sortFields) {
       return [];
     }
     return props.schemaBridge
       .getSubfields()
       .slice()
+      .filter((field) => {
+        if (!Array.isArray(props.omitFields)) return true;
+
+        return !props.omitFields.includes(field);
+      })
       .sort((a, b) => {
         const propsA = props.schemaBridge?.getProps(a);
         const propsB = props.schemaBridge?.getProps(b);
@@ -32,26 +43,34 @@ export const CustomAutoForm = forwardRef<{ fields: HTMLElement[] }, CustomAutoFo
 
   useImperativeHandle(forwardedRef, () => ({
     fields: fieldsRefs.current,
+    form: formRef.current,
   }));
 
   return (
     <AutoField.componentDetectorContext.Provider value={CustomAutoFieldDetector}>
       <AutoForm
+        ref={formRef}
         schema={props.schemaBridge}
         model={props.model}
         onChangeModel={props.onChangeModel}
+        onChange={props.onChange}
         data-testid={props['data-testid']}
         disabled={props.disabled}
       >
-        {sortedFieldsNames.map((field, index) => (
-          <AutoField
-            key={field}
-            name={field}
-            inputRef={(node: HTMLElement) => {
-              fieldsRefs.current[index] = node;
-            }}
-          />
-        ))}
+        {props.sortFields ? (
+          // For some forms, sorting its fields might be beneficial
+          sortedFieldsNames.map((field, index) => (
+            <AutoField
+              key={field}
+              name={field}
+              inputRef={(node: HTMLElement) => {
+                fieldsRefs.current[index] = node;
+              }}
+            />
+          ))
+        ) : (
+          <AutoFields omitFields={props.omitFields} />
+        )}
         <ErrorsField />
       </AutoForm>
     </AutoField.componentDetectorContext.Provider>
