f1de413b99cc4111e105137c6cac6224c0bc8cd1
tplevko <tplevko@redhat.com>
2024-02-21 09:20:52 +0100

    e2e(757) - add tests for support for kamelet beans

--- packages/ui-tests/cypress/e2e/designer/routeBeanConf.cy.ts
+++ packages/ui-tests/cypress/e2e/designer/routeBeanConf.cy.ts
@@ -3,70 +3,81 @@ describe('Test for node bean reference and configuration support', () => {
     cy.openHomePage();
   });
 
-  it('Beans - create a new bean in route using bean editor', () => {
-    cy.uploadFixture('flows/SqlBeansCR.yaml');
-    cy.openDesignPage();
+  const testData = [
+    { file: 'SqlBeansKamelet.yaml', dataSource: '#bean:{{test}}' },
+    { file: 'SqlBeansCR.yaml', dataSource: '#test' },
+  ];
+  testData.forEach((data) => {
+    it('Beans - create a new bean in route using bean editor ' + data.file, () => {
+      cy.uploadFixture('flows/' + data.file);
+      cy.openDesignPage();
 
-    cy.openStepConfigurationTab('sql');
-    cy.configureNewBeanReference('parameters.dataSource');
-    cy.get(`input[name="name"]`).clear().type('test');
-    cy.get(`input[name="type"]`).clear().type('org.acme');
+      cy.openStepConfigurationTab('sql');
+      cy.configureNewBeanReference('parameters.dataSource');
+      cy.get(`input[name="name"]`).clear().type('test');
+      cy.get(`input[name="type"]`).clear().type('org.acme');
 
-    cy.expandWrappedSection('properties');
-    cy.get('[data-testid="properties-add-string-property--btn"]').not(':hidden').first().click({ force: true });
-    cy.get('[data-testid="properties--placeholder-name-input"]').should('not.be.disabled');
-    cy.get('[data-testid="properties--placeholder-name-input"]').click({ force: true });
-    cy.get('[data-testid="properties--placeholder-name-input"]').clear().type('test');
+      cy.expandWrappedSection('properties');
+      cy.get('[data-testid="properties-add-string-property--btn"]').not(':hidden').first().click({ force: true });
+      cy.get('[data-testid="properties--placeholder-name-input"]').should('not.be.disabled');
+      cy.get('[data-testid="properties--placeholder-name-input"]').click({ force: true });
+      cy.get('[data-testid="properties--placeholder-name-input"]').clear().type('test');
 
-    cy.get('[data-testid="properties--placeholder-value-input"]').should('not.be.disabled');
-    cy.get('[data-testid="properties--placeholder-value-input"]').click({ force: true });
-    cy.get('[data-testid="properties--placeholder-value-input"]').clear().type('value');
+      cy.get('[data-testid="properties--placeholder-value-input"]').should('not.be.disabled');
+      cy.get('[data-testid="properties--placeholder-value-input"]').click({ force: true });
+      cy.get('[data-testid="properties--placeholder-value-input"]').clear().type('value');
 
-    cy.get('[data-testid="properties--placeholder-property-edit-confirm--btn"]').click({ force: true });
+      cy.get('[data-testid="properties--placeholder-property-edit-confirm--btn"]').click({ force: true });
 
-    cy.get('[data-testid="create-bean-btn"').click();
-    cy.closeStepConfigurationTab();
-    cy.openSourceCode();
+      cy.get('[data-testid="create-bean-btn"').click();
+      cy.closeStepConfigurationTab();
+      cy.openSourceCode();
 
-    // CHECK the bean was created in the code editor
-    cy.openSourceCode();
-    cy.checkCodeSpanLine('- beans:');
-    cy.checkCodeSpanLine('- name: test');
-    cy.checkCodeSpanLine('type: org.acme');
-    cy.checkCodeSpanLine('properties:');
-    cy.checkCodeSpanLine('test: value');
-    // CHECK the bean is referenced in the code editor
-    cy.checkCodeSpanLine('dataSource: "#test"');
+      // CHECK the bean was created in the code editor
+      cy.openSourceCode();
+      cy.checkCodeSpanLine('- name: test');
+      cy.checkCodeSpanLine('type: org.acme');
+      cy.checkCodeSpanLine('properties:');
+      cy.checkCodeSpanLine('test: value');
+      // CHECK the bean is referenced in the code editor
+      cy.checkCodeSpanLine('dataSource: "' + data.dataSource + '"');
+    });
   });
 
-  it('Beans - select existing bean in node form config', () => {
-    cy.openSourceCode();
-    cy.uploadFixture('flows/SqlBeansCR.yaml');
+  const newTestData = [
+    { file: 'SqlBeansKamelet.yaml', dataSource: '#bean:{{postgreSqlSource}}' },
+    { file: 'SqlBeansCR.yaml', dataSource: '#postgreSqlSource' },
+  ];
+  newTestData.forEach((data) => {
+    it('Beans - select existing bean in node form config ' + data.file, () => {
+      cy.openSourceCode();
+      cy.uploadFixture('flows/' + data.file);
 
-    cy.openDesignPage();
-    cy.openStepConfigurationTab('sql');
-    cy.configureBeanReference('parameters.dataSource', 'mysqlPostgreSqlSource');
-    cy.openSourceCode();
+      cy.openDesignPage();
+      cy.openStepConfigurationTab('sql');
+      cy.configureBeanReference('parameters.dataSource', data.dataSource);
+      cy.openSourceCode();
 
-    // CHECK the bean update was reflected in the code editor
-    cy.checkCodeSpanLine('dataSource: "#mysqlPostgreSqlSource"');
-  });
+      // CHECK the bean update was reflected in the code editor
+      cy.checkCodeSpanLine('dataSource: "' + data.dataSource + '"');
+    });
 
-  it('Beans - unselect selected bean', () => {
-    cy.openSourceCode();
-    cy.uploadFixture('flows/SqlBeansCR.yaml');
+    it('Beans - unselect selected bean', () => {
+      cy.openSourceCode();
+      cy.uploadFixture('flows/' + data.file);
 
-    cy.openDesignPage();
-    cy.openStepConfigurationTab('sql');
-    cy.configureBeanReference('parameters.dataSource', 'mysqlPostgreSqlSource');
-    cy.openSourceCode();
-    cy.checkCodeSpanLine('dataSource: "#mysqlPostgreSqlSource"');
+      cy.openDesignPage();
+      cy.openStepConfigurationTab('sql');
+      cy.configureBeanReference('parameters.dataSource', data.dataSource);
+      cy.openSourceCode();
+      cy.checkCodeSpanLine('dataSource: "' + data.dataSource + '"');
 
-    cy.openDesignPage();
-    cy.openStepConfigurationTab('sql');
-    cy.deselectNodeBean('parameters.dataSource');
-    cy.openSourceCode();
-    cy.checkCodeSpanLine('dataSource: "#mysqlPostgreSqlSource"', 0);
+      cy.openDesignPage();
+      cy.openStepConfigurationTab('sql');
+      cy.deselectNodeBean('parameters.dataSource');
+      cy.openSourceCode();
+      cy.checkCodeSpanLine('dataSource: "' + data.dataSource + '"', 0);
+    });
   });
 
   // blocked by https://github.com/KaotoIO/kaoto-next/issues/558
@@ -74,7 +85,7 @@ describe('Test for node bean reference and configuration support', () => {
     cy.uploadFixture('flows/SqlBeansCR.yaml');
     cy.openDesignPage();
     cy.openStepConfigurationTab('sql');
-    cy.configureBeanReference('parameters.dataSource', 'mysqlPostgreSqlSource');
+    cy.configureBeanReference('parameters.dataSource', 'postgreSqlSource');
 
     cy.openBeans();
     // Remove the bean
@@ -82,6 +93,6 @@ describe('Test for node bean reference and configuration support', () => {
     cy.get('[data-testid="metadata-row-1"]').should('not.exist');
     cy.openDesignPage();
     cy.openStepConfigurationTab('sql');
-    cy.get(`div[data-fieldname="parameters.dataSource"] input[value="#mysqlPostgreSqlSource"]`).should('not.exist');
+    cy.get(`div[data-fieldname="parameters.dataSource"] input[value="#postgreSqlSource"]`).should('not.exist');
   });
 });
