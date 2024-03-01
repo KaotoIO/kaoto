4fd6cc2cf9e0f6094db3333ed5907acbc58637c4
Tomohisa Igarashi <tm.igarashi@gmail.com>
2024-02-29 21:33:21 -0500

    trying dndkit

--- packages/poc-dnd-line/src/DocumentField.tsx
+++ packages/poc-dnd-line/src/DocumentField.tsx
@@ -1,5 +1,6 @@
 import { AccordionContent, AccordionItem, AccordionToggle } from '@patternfly/react-core';
 import { FunctionComponent, useCallback, useState } from 'react';
+import { useDraggable, useDroppable } from '@dnd-kit/core';
 
 type DocumentFieldProps = {
   field: any;
@@ -7,7 +8,25 @@ type DocumentFieldProps = {
 };
 export const DocumentField: FunctionComponent<DocumentFieldProps> = ({ field, initialExpanded = false }) => {
   const [isExpanded, setIsExpanded] = useState<boolean>(initialExpanded);
-
+  const { isOver, setNodeRef: setDroppableNodeRef } = useDroppable({
+    id: 'droppable',
+  });
+  const droppableStyle = {
+    color: isOver ? 'green' : undefined,
+  };
+  const {
+    attributes,
+    listeners,
+    setNodeRef: setDraggableNodeRef,
+    transform,
+  } = useDraggable({
+    id: 'draggable',
+  });
+  const draggableStyle = transform
+    ? {
+        transform: `translate3d(${transform.x}px, ${transform.y}px, 0)`,
+      }
+    : undefined;
   const renderField = useCallback(
     (field: any) => {
       return (
@@ -15,7 +34,11 @@ export const DocumentField: FunctionComponent<DocumentFieldProps> = ({ field, in
           {field.fields && field.fields.length !== 0 ? (
             <>
               <AccordionToggle onClick={() => setIsExpanded(!isExpanded)} isExpanded={isExpanded} id={field.name}>
-                {field.name}
+                <div ref={setDroppableNodeRef} style={droppableStyle}>
+                  <div ref={setDraggableNodeRef} style={draggableStyle} {...listeners} {...attributes}>
+                    {field.name}
+                  </div>
+                </div>
               </AccordionToggle>
               <AccordionContent isHidden={!isExpanded} id={field.name}>
                 {field.fields.map((f: any) => (
@@ -24,7 +47,13 @@ export const DocumentField: FunctionComponent<DocumentFieldProps> = ({ field, in
               </AccordionContent>
             </>
           ) : (
-            <AccordionContent id={field.name}>{field.name}</AccordionContent>
+            <AccordionContent id={field.name}>
+              <div ref={setDroppableNodeRef} style={droppableStyle}>
+                <div ref={setDraggableNodeRef} style={draggableStyle} {...listeners} {...attributes}>
+                  {field.name}
+                </div>
+              </div>
+            </AccordionContent>
           )}
         </AccordionItem>
       );
--- packages/poc-dnd-line/src/main.tsx
+++ packages/poc-dnd-line/src/main.tsx
@@ -1,8 +1,12 @@
 import '@patternfly/react-core/dist/styles/base.css'; // This import needs to be first
 import { createRoot } from 'react-dom/client';
-import { DnDCodeSample } from './dnd';
 import { SourceTarget } from './SourceTarget';
+import { DndContext } from '@dnd-kit/core';
 
 const container = document.getElementById('root');
 const root = createRoot(container!);
-root.render(<SourceTarget></SourceTarget>);
+root.render(
+  <DndContext>
+    <SourceTarget></SourceTarget>
+  </DndContext>,
+);
