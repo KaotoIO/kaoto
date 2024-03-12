a6317d87777942b90050c9e381b61f3ce9a07532
Ricardo M <lordrip@gmail.com>
2024-03-12 23:11:19 +0100

    feat(build): Add GIT info to the build stage
    This commit provide information from the last commit when building the
    application.
    
    For the npm package, it overrides the `version.json` file during build time. For the web application, it provides the same information using the `define` property form the vite configuration.

--- packages/ui/src/global.d.ts
+++ packages/ui/src/global.d.ts
@@ -1,3 +1,6 @@
 declare module '*.png';
 declare module '*.svg';
 declare let NODE_ENV: string;
+declare let __GIT_HASH: string;
+declare let __GIT_DATE: string;
+declare let __KAOTO_VERSION: string;
