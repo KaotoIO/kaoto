KaotoIO/kaoto-next

fix(278): Group parameters in the configuration panel

packages/camel-catalog/kaoto-camel-catalog-maven-plugin/src/main/java/io/kaoto/camelcatalog/CamelCatalogProcessor.java
@@ -104,6 +104,7 @@ public class CamelCatalogProcessor {
             var property = (ObjectNode) propertyEntry.getValue();
             var propertySchema = answerProperties.withObject("/" + propertyName);
             if (property.has("displayName")) propertySchema.put("title", property.get("displayName").asText());
+            if (property.has("label")) propertySchema.put("labels", property.get("label").asText());
             if (property.has("description")) propertySchema.put("description", property.get("description").asText());
             var propertyType = "string";
             if (property.has("type")) {

packages/ui/src/components/Form/customField/CustomNestField.test.tsx
@@ -1,0 +1,128 @@
+import { fireEvent, render, screen } from '@testing-library/react';
+import { act } from 'react-dom/test-utils';
+
+import { AutoField } from '@kaoto-next/uniforms-patternfly';
+
+import { AutoForm } from 'uniforms';
+import { CustomAutoFieldDetector } from '../CustomAutoField';
+import { SchemaService } from '../schema.service';
+import { CustomNestField } from './CustomNestField';
+
+describe('CustomNestField', () => {
+  const mockSchema = {
+    title: 'Test',
+    type: 'object',
+    additionalProperties: false,
+    properties: {
+      parameters: {
+        type: 'object',
+        title: 'Endpoint Properties',
+        description: 'Endpoint properties description',
+        properties: {
+          timerName: {
+            title: 'Timer Name',
+            labels: '',
+            description: 'The name of the timer',
+            type: 'string',
+            deprecated: false,
+          },
+          pattern: {
+            title: 'Pattern',
+            labels: 'advanced',
+            description:
+              'Allows you to specify a custom Date pattern to use for setting the time option using URI syntax.',
+            type: 'string',
+            deprecated: false,
+          },
+        },
+      },
+    },
+  };
+
+  const schemaService = new SchemaService();
+  const schemaBridge = schemaService.getSchemaBridge(mockSchema);
+
+  it('should render the component', () => {
+    render(
+      <AutoField.componentDetectorContext.Provider value={CustomAutoFieldDetector}>
+        <AutoForm schema={schemaBridge!}>
+          <CustomNestField name="parameters" />
+        </AutoForm>
+      </AutoField.componentDetectorContext.Provider>,
+    );
+    const inputTimerElement = screen
+      .getAllByRole('textbox')
+      .filter((textbox) => textbox.getAttribute('label') === 'Timer Name');
+    expect(inputTimerElement).toHaveLength(1);
+    const inputPatternElement = screen
+      .getAllByRole('textbox')
+      .filter((textbox) => textbox.getAttribute('label') === 'Pattern');
+    expect(inputPatternElement).toHaveLength(0);
+    const buttonElement = screen.getByRole('button', { name: 'Advanced properties' });
+    expect(buttonElement).toBeInTheDocument();
+  });
+
+  it('should display the advanced properties', async () => {
+    render(
+      <AutoField.componentDetectorContext.Provider value={CustomAutoFieldDetector}>
+        <AutoForm schema={schemaBridge!}>
+          <CustomNestField name="parameters" />
+        </AutoForm>
+      </AutoField.componentDetectorContext.Provider>,
+    );
+    const buttonElement = screen.getByRole('button', { name: 'Advanced properties' });
+    await act(async () => {
+      fireEvent.click(buttonElement);
+    });
+    const inputPatternElement = screen
+      .getAllByRole('textbox')
+      .filter((textbox) => textbox.getAttribute('label') === 'Pattern');
+    expect(inputPatternElement).toHaveLength(1);
+  });
+});
+
+describe('CustomNestField', () => {
+  const mockSchema = {
+    title: 'Test',
+    type: 'object',
+    additionalProperties: false,
+    properties: {
+      parameters: {
+        type: 'object',
+        title: 'Endpoint Properties',
+        description: 'Endpoint properties description',
+        properties: {
+          timerName: {
+            title: 'Timer Name',
+            labels: '',
+            description: 'The name of the timer',
+            type: 'string',
+            deprecated: false,
+          },
+        },
+      },
+    },
+  };
+
+  const schemaService = new SchemaService();
+  const schemaBridge = schemaService.getSchemaBridge(mockSchema);
+
+  it('should not render the advanced properties button if no advanced properties are provided', () => {
+    render(
+      <AutoField.componentDetectorContext.Provider value={CustomAutoFieldDetector}>
+        <AutoForm schema={schemaBridge!}>
+          <CustomNestField name="parameters" />
+        </AutoForm>
+      </AutoField.componentDetectorContext.Provider>,
+    );
+    const inputTimerElement = screen
+      .getAllByRole('textbox')
+      .filter((textbox) => textbox.getAttribute('label') === 'Timer Name');
+    expect(inputTimerElement).toHaveLength(1);
+
+    const advancedButton = screen
+      .getAllByRole('button')
+      .filter((button) => button.textContent === 'Advanced properties');
+    expect(advancedButton).toHaveLength(0);
+  });
+});

KaotoIO/kaoto

packages/camel-catalog/kaoto-camel-catalog-maven-plugin/src/main/java/io/kaoto/camelcatalog/CamelCatalogProcessor.java
@@ -104,6 +104,7 @@ public class CamelCatalogProcessor {
             var property = (ObjectNode) propertyEntry.getValue();
             var propertySchema = answerProperties.withObject("/" + propertyName);
             if (property.has("displayName")) propertySchema.put("title", property.get("displayName").asText());
+            if (property.has("group")) propertySchema.put("group", property.get("group").asText());
             if (property.has("description")) propertySchema.put("description", property.get("description").asText());
             var propertyType = "string";
             if (property.has("type")) {

packages/ui/src/components/Form/customField/CustomNestField.test.tsx
@@ -1,0 +1,132 @@
+import { fireEvent, render, screen } from '@testing-library/react';
+import { act } from 'react-dom/test-utils';
+
+import { AutoField } from '@kaoto-next/uniforms-patternfly';
+
+import { AutoForm } from 'uniforms';
+import { CustomAutoFieldDetector } from '../CustomAutoField';
+import { SchemaService } from '../schema.service';
+import { CustomNestField } from './CustomNestField';
+
+describe('CustomNestField', () => {
+  const mockSchema = {
+    title: 'Test',
+    type: 'object',
+    additionalProperties: false,
+    properties: {
+      parameters: {
+        type: 'object',
+        title: 'Endpoint Properties',
+        description: 'Endpoint properties description',
+        properties: {
+          timerName: {
+            title: 'Timer Name',
+            group: 'common',
+            description: 'The name of the timer',
+            type: 'string',
+          },
+          pattern: {
+            title: 'Pattern',
+            group: 'advanced',
+            description:
+              'Allows you to specify a custom Date pattern to use for setting the time option using URI syntax.',
+            type: 'string',
+          },
+          secret: {
+            title: 'Secret',
+            group: 'secret',
+            description: 'The secret',
+            type: 'string',
+          },
+        },
+      },
+    },
+  };
+
+  const schemaService = new SchemaService();
+  const schemaBridge = schemaService.getSchemaBridge(mockSchema);
+
+  it('should render the component', () => {
+    render(
+      <AutoField.componentDetectorContext.Provider value={CustomAutoFieldDetector}>
+        <AutoForm schema={schemaBridge!}>
+          <CustomNestField name="parameters" />
+        </AutoForm>
+      </AutoField.componentDetectorContext.Provider>,
+    );
+    const inputTimerElement = screen
+      .getAllByRole('textbox')
+      .filter((textbox) => textbox.getAttribute('label') === 'Timer Name');
+    expect(inputTimerElement).toHaveLength(1);
+    const inputPatternElement = screen
+      .getAllByRole('textbox')
+      .filter((textbox) => textbox.getAttribute('label') === 'Pattern');
+    expect(inputPatternElement).toHaveLength(0);
+    const advancedProperties = screen.getByRole('button', { name: 'advanced properties' });
+    const secretProperties = screen.getByRole('button', { name: 'secret properties' });
+    expect(advancedProperties).toBeInTheDocument();
+    expect(secretProperties).toBeInTheDocument();
+  });
+
+  it('should display the advanced properties', async () => {
+    render(
+      <AutoField.componentDetectorContext.Provider value={CustomAutoFieldDetector}>
+        <AutoForm schema={schemaBridge!}>
+          <CustomNestField name="parameters" />
+        </AutoForm>
+      </AutoField.componentDetectorContext.Provider>,
+    );
+    const buttonElement = screen.getByRole('button', { name: 'advanced properties' });
+    await act(async () => {
+      fireEvent.click(buttonElement);
+    });
+    const inputPatternElement = screen
+      .getAllByRole('textbox')
+      .filter((textbox) => textbox.getAttribute('label') === 'Pattern');
+    expect(inputPatternElement).toHaveLength(1);
+  });
+});
+
+describe('CustomNestField', () => {
+  const mockSchema = {
+    title: 'Test',
+    type: 'object',
+    additionalProperties: false,
+    properties: {
+      parameters: {
+        type: 'object',
+        title: 'Endpoint Properties',
+        description: 'Endpoint properties description',
+        properties: {
+          timerName: {
+            title: 'Timer Name',
+            description: 'The name of the timer',
+            type: 'string',
+          },
+        },
+      },
+    },
+  };
+
+  const schemaService = new SchemaService();
+  const schemaBridge = schemaService.getSchemaBridge(mockSchema);
+
+  it('should not render the advanced properties button if no advanced properties are provided', () => {
+    render(
+      <AutoField.componentDetectorContext.Provider value={CustomAutoFieldDetector}>
+        <AutoForm schema={schemaBridge!}>
+          <CustomNestField name="parameters" />
+        </AutoForm>
+      </AutoField.componentDetectorContext.Provider>,
+    );
+    const inputTimerElement = screen
+      .getAllByRole('textbox')
+      .filter((textbox) => textbox.getAttribute('label') === 'Timer Name');
+    expect(inputTimerElement).toHaveLength(1);
+
+    const advancedButton = screen
+      .getAllByRole('button')
+      .filter((button) => button.textContent === 'advanced properties');
+    expect(advancedButton).toHaveLength(0);
+  });
+});