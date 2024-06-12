5c7921b210283214804095b33f4acbc058d4e46a
tplevko <tplevko@redhat.com>
2024-06-12 12:04:41 +0200

    e2e - preserving modeline after pipe config

---
+++ packages/ui-tests/cypress/e2e/designer/basicNodeActions/stepDisable.cy.ts
@@ -0,0 +1,17 @@
+describe('Tests for Design page', () => {
+  beforeEach(() => {
+    cy.openHomePage();
+  });
+
+  it('Design - disable steps in CamelRoute', () => {
+    cy.uploadFixture('flows/camelRoute/basic.yaml');
+    cy.openDesignPage();
+
+    cy.selectDisableNode('setHeader');
+    cy.openStepConfigurationTab('setHeader');
+    cy.checkConfigCheckboxObject('disabled', true);
+
+    cy.openSourceCode();
+    cy.checkCodeSpanLine('disabled: true', 1);
+  });
+});
---
+++ packages/ui-tests/cypress/e2e/designer/sidepanelConfig/modelineConf.cy.ts
@@ -0,0 +1,17 @@
+describe('Tests for modeline', () => {
+  beforeEach(() => {
+    cy.openHomePage();
+  });
+
+  it('Preserve modeline in pipe config after pipe source was changed', () => {
+    cy.uploadFixture('flows/pipe/modeline.yaml');
+    cy.openDesignPage();
+
+    cy.removeNodeByName('https');
+    cy.openStepConfigurationTab('kamelet:log-sink');
+    cy.get(`input[name="parameters.showProperties"]`).check();
+
+    cy.openSourceCode();
+    cy.checkCodeSpanLine('# camel-k: dependency=camel:aws-secrets-manager', 1);
+  });
+});
