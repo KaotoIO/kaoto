75f424f3ea939b8bb7a27be26348c869f9190b8a
tplevko <tplevko@redhat.com>
2024-08-01 13:32:07 +0200

    e2e(KTO-450): add test for settings switch for node label

---
+++ packages/ui-tests/cypress/e2e/designer/settings/switchDescriptionId.cy.ts
@@ -0,0 +1,38 @@
+describe('Tests for switching description and ID in settings page', () => {
+  beforeEach(() => {
+    cy.openHomePage();
+  });
+
+  // Reset nodeLabel to description
+  after(() => {
+    cy.openSettings();
+    cy.selectInTypeaheadField('nodeLabel', 'description');
+    cy.get('[data-testid="settings-form-save-btn"]').click();
+    cy.waitSchemasLoading();
+  });
+
+  it('User switches node label type description and ID to be displayed in canvas', () => {
+    cy.uploadFixture('flows/camelRoute/idDescriptionSettings.yaml');
+    cy.openSettings();
+    cy.selectInTypeaheadField('nodeLabel', 'id');
+    cy.get('[data-testid="settings-form-save-btn"]').click();
+    cy.waitSchemasLoading();
+    cy.openDesignPage();
+
+    cy.checkNodeExist('idTimer', 1);
+    cy.checkNodeExist('descriptionSetHeader', 1);
+    cy.checkNodeExist('idMarshal', 1);
+    cy.checkNodeExist('log', 1);
+
+    cy.openSettings();
+    cy.selectInTypeaheadField('nodeLabel', 'description');
+    cy.get('[data-testid="settings-form-save-btn"]').click();
+    cy.waitSchemasLoading();
+    cy.openDesignPage();
+
+    cy.checkNodeExist('descriptionTimer', 1);
+    cy.checkNodeExist('descriptionSetHeader', 1);
+    cy.checkNodeExist('marshal', 1);
+    cy.checkNodeExist('log', 1);
+  });
+});
