cc78d42e166f76346c07065c9010490051d3bd7d
tplevko <tplevko@redhat.com>
2024-08-07 11:37:58 +0200

    fix storybook NewBeanModal story

--- packages/ui-tests/stories/canvas/NewBeanModal.stories.tsx
+++ packages/ui-tests/stories/canvas/NewBeanModal.stories.tsx
@@ -1,37 +1,54 @@
+import { KaotoSchemaDefinition, NewBeanModal } from '@kaoto/kaoto/testing';
 import { Meta, StoryFn } from '@storybook/react';
-import {
-  CatalogLoaderProvider,
-  CatalogSchemaLoader,
-  SchemasLoaderProvider,
-  NewBeanModal,
-  RuntimeProvider,
-} from '@kaoto/kaoto/testing';
+import { useState } from 'react';
+
+const beanSchema: KaotoSchemaDefinition['schema'] = {
+  title: 'Bean Factory',
+  description: 'Define custom beans that can be used in your Camel routes and in general.',
+  type: 'object',
+  additionalProperties: false,
+  properties: {
+    builderClass: {
+      type: 'string',
+      title: 'Builder Class',
+      description:
+        'Fully qualified class name of builder class to use for creating and configuring the bean. The builder will use the properties values to configure the bean.',
+    },
+    name: {
+      type: 'string',
+      title: 'Name',
+      description: 'The name of the bean (bean id)',
+    },
+    type: {
+      type: 'string',
+      title: 'Type',
+      description: 'The class name (fully qualified) of the bean',
+    },
+  },
+  required: ['name', 'type'],
+};
 
 export default {
   title: 'Canvas/NewBeanModal',
   component: NewBeanModal,
-  decorators: [
-    (Story: StoryFn) => (
-      <RuntimeProvider catalogUrl={CatalogSchemaLoader.DEFAULT_CATALOG_PATH}>
-        <SchemasLoaderProvider>
-          <CatalogLoaderProvider>
-            <Story />
-          </CatalogLoaderProvider>
-        </SchemasLoaderProvider>
-      </RuntimeProvider>
-    ),
-  ],
+  decorators: [(Story: StoryFn) => <Story />],
 } as Meta<typeof NewBeanModal>;
 
 const Template: StoryFn<typeof NewBeanModal> = (args) => {
-  return <NewBeanModal {...args} />;
+  const [isModalOpen, setIsModalOpen] = useState(true);
+  const handleClose = () => setIsModalOpen(!isModalOpen);
+  return (
+    <>
+      <button onClick={() => setIsModalOpen(true)}>Open New Bean Modal</button>
+      <NewBeanModal {...args} onCancelCreateBean={handleClose} isOpen={isModalOpen} />
+    </>
+  );
 };
 
 export const Default = Template.bind({});
 Default.args = {
-  isOpen: true,
-  onCancelCreateBean: () => {},
+  beanSchema: beanSchema,
   onCreateBean: () => {},
-  propertyTitle: 'sample',
+  propertyTitle: 'Bean Factory',
   javaType: 'org.apache.camel.spi.ExceptionHandler',
 };
