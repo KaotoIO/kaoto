b5b069d06a79a1013bc8b47f54b989abea439f11
Ricardo M. <lordrip@gmail.com>
2024-08-20 15:00:02 -0400

    chore(test): Add @swc/jest to reduce heap usage

--- packages/ui/jest.config.ts
+++ packages/ui/jest.config.ts
@@ -189,6 +189,10 @@ export default {
 
   // A map from regular expressions to paths to transformers
   // transform: undefined,
+  transform: {
+    '^.+\\.(t|j)sx?$': '@swc/jest',
+  },
+  extensionsToTreatAsEsm: ['.ts', '.tsx'],
 
   // An array of regexp pattern strings that are matched against all source file paths, matched files will skip transformation
   transformIgnorePatterns: [
