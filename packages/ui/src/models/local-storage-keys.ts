7d9abe095f5aabf501b8349bee749c19128f3f00
tplevko <tplevko@redhat.com>
2024-09-02 09:43:32 +0200

    Add sidebar width preserve in local storage

--- packages/ui/src/models/local-storage-keys.ts
+++ packages/ui/src/models/local-storage-keys.ts
@@ -2,6 +2,7 @@ export const enum LocalStorageKeys {
   SourceCode = 'sourceCode',
   CatalogLayout = 'catalogLayout',
   CanvasLayout = 'canvasLayout',
+  CanvasSidebarWidth = 'canvasSidebarWidth',
   NavigationExpanded = 'navigationExpanded',
   SelectedCatalog = 'selectedCatalog',
   Settings = 'settings',
