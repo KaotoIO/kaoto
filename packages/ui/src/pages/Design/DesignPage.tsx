845ba638d368874fdf24ba92d9206ac8b89b6483
Ricardo M. <lordrip@gmail.com>
2024-09-05 00:25:32 +0200

    chore(VSCode): Refactor KaotoEditor structure
    Currently, the `DesignPage` component is duplicated to serve the
    multiplying architecture and the webapp.
    
    In addition to that, for the multiplying architecture, we're using a
    React render approach to navigate between different tabs, whereas in the
    webapp we use a Router.
    
    This commit removes the `Tabs` folder and use the `DesignPage` component directly to reduce the duplication.
    
    In addition to that, a Router has been introduced to navigate between
    the tabs, this as a cleanup for the upcoming tasks.

--- packages/ui/src/pages/Design/DesignPage.tsx
+++ packages/ui/src/pages/Design/DesignPage.tsx
@@ -1,26 +1,19 @@
-import { FunctionComponent, useContext } from 'react';
+import { FunctionComponent, ReactNode, useContext } from 'react';
 import { Visualization } from '../../components/Visualization';
 import { CatalogModalProvider } from '../../providers/catalog-modal.provider';
+import { DeleteModalContextProvider } from '../../providers/delete-modal.provider';
 import { EntitiesContext } from '../../providers/entities.provider';
 import './DesignPage.scss';
-import { ReturnToSourceCodeFallback } from './ReturnToSourceCodeFallback';
-import { DeleteModalContextProvider } from '../../providers/delete-modal.provider';
 
-export const DesignPage: FunctionComponent = () => {
+export const DesignPage: FunctionComponent<{ fallback?: ReactNode }> = (props) => {
   const entitiesContext = useContext(EntitiesContext);
   const visualEntities = entitiesContext?.visualEntities ?? [];
 
   return (
-    <div className="canvas-page">
-      <CatalogModalProvider>
-        <DeleteModalContextProvider>
-          <Visualization
-            className="canvas-page__canvas"
-            entities={visualEntities}
-            fallback={<ReturnToSourceCodeFallback />}
-          />
-        </DeleteModalContextProvider>
-      </CatalogModalProvider>
-    </div>
+    <CatalogModalProvider>
+      <DeleteModalContextProvider>
+        <Visualization className="canvas-page" entities={visualEntities} fallback={props.fallback} />
+      </DeleteModalContextProvider>
+    </CatalogModalProvider>
   );
 };
