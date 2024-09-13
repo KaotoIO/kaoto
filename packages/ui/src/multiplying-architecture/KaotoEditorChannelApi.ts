12a042a27c71b1ab7c51dde8264e6dd9bb7a2039
Ricardo M. <lordrip@gmail.com>
2024-09-13 14:36:58 +0200

    feat(VSCode): Define metadata API

--- packages/ui/src/multiplying-architecture/KaotoEditorChannelApi.ts
+++ packages/ui/src/multiplying-architecture/KaotoEditorChannelApi.ts
@@ -12,4 +12,30 @@ export interface KaotoEditorChannelApi extends KogitoEditorChannelApi {
    * Returns the Kaoto VSCode settings defined.
    */
   getVSCodeKaotoSettings(): Promise<ISettingsModel>;
+
+  /**
+   * Get metadata querying a Kaoto metadata file.
+   * @param key The key to retrieve the metadata from the Kaoto metadata file
+   */
+  getMetadata<T>(key: string): Promise<T | undefined>;
+
+  /**
+   * Save metadata to a Kaoto metadata file.
+   * @param key The key to set the metadata
+   * @param metadata The metadata to be saved
+   */
+  setMetadata<T>(key: string, metadata: T): Promise<void>;
+
+  /**
+   * Retrieve resource content
+   * @param path The path of the resource
+   */
+  getResourceContent(path: string): Promise<string | undefined>;
+
+  /**
+   * Save resource content
+   * @param path The path of the resource
+   * @param content The content to be saved
+   */
+  saveResourceContent(path: string, content: string): Promise<void>;
 }
