952134e01794decfe70f65219b914da3b814074d
Tomohisa Igarashi <tm.igarashi@gmail.com>
2024-10-08 19:41:40 -0400

    fix: Enhance confirmation modal provider to offer customizable options/buttons
    Fixes: #1537

--- packages/ui/src/providers/action-confirmaton-modal.provider.test.tsx
+++ packages/ui/src/providers/action-confirmaton-modal.provider.test.tsx
@@ -1,11 +1,13 @@
 import { act, fireEvent, render, screen, waitFor } from '@testing-library/react';
 import { FunctionComponent, useContext } from 'react';
 import {
+  ActionConfirmationButtonOption,
   ActionConfirmationModalContext,
   ActionConfirmationModalContextProvider,
 } from './action-confirmation-modal.provider';
+import { ButtonVariant } from '@patternfly/react-core';
 
-let actionConfirmationResult: boolean | undefined;
+let actionConfirmationResult: number | undefined;
 
 describe('ActionConfirmationModalProvider', () => {
   beforeEach(() => {
@@ -26,7 +28,7 @@ describe('ActionConfirmationModalProvider', () => {
     fireEvent.click(confirmButton);
 
     // Wait for actionConfirmation promise to resolve
-    await waitFor(() => expect(actionConfirmationResult).toEqual(true));
+    await waitFor(() => expect(actionConfirmationResult).toEqual(1));
   });
 
   it('calls actionConfirmation with false when Cancel button is clicked', async () => {
@@ -43,7 +45,7 @@ describe('ActionConfirmationModalProvider', () => {
     fireEvent.click(cancelButton);
 
     // Wait for actionConfirmation promise to resolve
-    await waitFor(() => expect(actionConfirmationResult).toEqual(false));
+    await waitFor(() => expect(actionConfirmationResult).toEqual(0));
   });
 
   it('should allow consumers to update the modal title and text', () => {
@@ -64,11 +66,79 @@ describe('ActionConfirmationModalProvider', () => {
     expect(wrapper.queryByText('Custom title')).toBeInTheDocument;
     expect(wrapper.queryByText('Custom text')).toBeInTheDocument;
   });
+
+  it('should show 3 options to choose', async () => {
+    const wrapper = render(
+      <ActionConfirmationModalContextProvider>
+        <TestComponent
+          title="Custom title"
+          text="Custom text"
+          additionalModalText="Additional text is added in the modal description"
+          buttonOptions={[
+            {
+              index: 1,
+              buttonText: 'Delete the step, and delete the file(s)',
+              variant: ButtonVariant.danger,
+            },
+            {
+              index: 2,
+              buttonText: 'Delete the step, but keep the file(s)',
+              variant: ButtonVariant.secondary,
+              isDanger: true,
+            },
+          ]}
+        />
+      </ActionConfirmationModalContextProvider>,
+    );
+
+    act(() => {
+      const deleteButton = wrapper.getByText('Delete');
+      fireEvent.click(deleteButton);
+    });
+    const modalDialog = wrapper.getByRole('dialog');
+    expect(modalDialog.textContent).toContain('Additional text is added in the modal description');
+    act(() => {
+      const cancelButton = wrapper.getByTestId('action-confirmation-modal-btn-0');
+      expect(cancelButton.textContent).toEqual('Cancel');
+      fireEvent.click(cancelButton);
+    });
+    await waitFor(() => {
+      expect(actionConfirmationResult).toEqual(0);
+    });
+
+    act(() => {
+      const deleteButton = wrapper.getByText('Delete');
+      fireEvent.click(deleteButton);
+    });
+    act(() => {
+      const deleteStepAndFileButton = wrapper.getByTestId('action-confirmation-modal-btn-1');
+      expect(deleteStepAndFileButton.textContent).toEqual('Delete the step, and delete the file(s)');
+      fireEvent.click(deleteStepAndFileButton);
+    });
+    await waitFor(() => {
+      expect(actionConfirmationResult).toEqual(1);
+    });
+
+    act(() => {
+      const deleteButton = wrapper.getByText('Delete');
+      fireEvent.click(deleteButton);
+    });
+    act(() => {
+      const deleteStepOnlyButton = wrapper.getByTestId('action-confirmation-modal-btn-2');
+      expect(deleteStepOnlyButton.textContent).toEqual('Delete the step, but keep the file(s)');
+      fireEvent.click(deleteStepOnlyButton);
+    });
+    await waitFor(() => {
+      expect(actionConfirmationResult).toEqual(2);
+    });
+  });
 });
 
 interface TestComponentProps {
   title: string;
   text: string;
+  additionalModalText?: string;
+  buttonOptions?: ActionConfirmationButtonOption[];
 }
 
 const TestComponent: FunctionComponent<TestComponentProps> = (props) => {
