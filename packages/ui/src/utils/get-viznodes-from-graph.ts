d985f8271a1aa56040e9a355c1d3ecaabeafb032
Ricardo M. <lordrip@gmail.com>
2024-10-14 21:24:16 +0000

    feat(IVizNode): Get All Visualization nodes
    A frequent use case for the Canvas is to identify nodes, unfortunately, there's no easy way to find nodes using a predicate.
    
    This commit allows finding nodes given a predicate or retrieving all nodes if no predicate has been provided.
    
    * How to use?
    ```typescript
    // First we obtain the `VisualizationController`
    const controller = useVisualizationController();
    
    // Second, we define a predicate
    const predicate = (vizNode: IVisualizationNode) => {
      return vizNode.getComponentSchema()?.definition?.disabled;
    };
    
    // Lastly, we use the `getVisualizationNodesFromGraph` function to find the nodes
    const vizNodes = getVisualizationNodesFromGraph(visualizationController.getGraph(), predicate);
    ```

---
+++ packages/ui/src/utils/get-viznodes-from-graph.ts
@@ -0,0 +1,50 @@
+/*
+ * Copyright (C) 2024 Red Hat, Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *         http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+import type { Graph, GraphElement } from '@patternfly/react-topology';
+import { CanvasNode } from '../components/Visualization/Canvas/canvas.models';
+import { IVisualizationNode } from '../models/visualization/base-visual-entity';
+import { isDefined } from './is-defined';
+
+const getVisualizationNodeFromCanvasNode = (
+  node: GraphElement<CanvasNode, CanvasNode['data']>,
+  predicate: (vizNode: IVisualizationNode) => boolean,
+  accumulator: IVisualizationNode[],
+): IVisualizationNode[] => {
+  const vizNode = node.getData()?.vizNode;
+  if (isDefined(vizNode) && predicate(vizNode)) {
+    accumulator.push(vizNode);
+  }
+
+  node.getChildren().forEach((child) => {
+    getVisualizationNodeFromCanvasNode(child, predicate, accumulator);
+  });
+
+  return accumulator;
+};
+
+export const getVisualizationNodesFromGraph = (
+  graph: Graph,
+  predicate: (vizNode: IVisualizationNode) => boolean = () => true,
+): IVisualizationNode[] => {
+  const vizNodes: IVisualizationNode[] = [];
+
+  graph.getNodes().forEach((node) => {
+    getVisualizationNodeFromCanvasNode(node, predicate, vizNodes);
+  });
+
+  return vizNodes;
+};
