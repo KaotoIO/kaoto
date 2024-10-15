15bffb1a69ab0347bf622483b703549489e61682
tplevko <tplevko@redhat.com>
2024-10-15 15:12:36 +0200

    e2e(1568): test for EnableAll menu item

--- packages/ui-tests/cypress/e2e/designer/basicNodeActions/stepDisable.cy.ts
+++ packages/ui-tests/cypress/e2e/designer/basicNodeActions/stepDisable.cy.ts
@@ -15,4 +15,29 @@ describe('Tests for Design page', () => {
     cy.openSourceCode();
     cy.checkCodeSpanLine('disabled: true', 1);
   });
+
+  it('Design - disable and enable multiple steps in CamelRoute', () => {
+    cy.uploadFixture('flows/camelRoute/basic.yaml');
+    cy.openDesignPage();
+
+    cy.selectDisableNode('setHeader');
+    cy.selectDisableNode('marshal');
+    cy.selectDisableNode('log');
+
+    cy.openStepConfigurationTab('setHeader');
+    cy.selectFormTab('All');
+    cy.checkConfigCheckboxObject('disabled', true);
+    cy.openStepConfigurationTab('marshal');
+    cy.selectFormTab('All');
+    cy.checkConfigCheckboxObject('disabled', true);
+    cy.openStepConfigurationTab('log');
+    cy.selectFormTab('All');
+    cy.checkConfigCheckboxObject('disabled', true);
+
+    cy.openDesignPage();
+    cy.selectEnableAllNodes('setHeader');
+
+    cy.openSourceCode();
+    cy.checkCodeSpanLine('disabled: true', 0);
+  });
 });
