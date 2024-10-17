c085d0544f1b11f07b8c75ccd66d95f9e3faa6de
Ricardo M. <lordrip@gmail.com>
2024-10-17 14:49:47 +0200

    chore(Canvas): Extract TargetAnchor

--- packages/ui/src/components/Visualization/Custom/Group/CustomGroupExpanded.tsx
+++ packages/ui/src/components/Visualization/Custom/Group/CustomGroupExpanded.tsx
@@ -1,11 +1,9 @@
 import {
-  AbstractAnchor,
   AnchorEnd,
   CollapsibleGroupProps,
   GROUPS_LAYER,
   Layer,
   Node,
-  Point,
   Rect,
   WithContextMenuProps,
   WithDndDropProps,
@@ -18,6 +16,7 @@ import { FunctionComponent, useRef } from 'react';
 import { CollapseButton } from './CollapseButton';
 import { ContextMenuButton } from './ContextMenuButton';
 import { CustomGroupProps } from './Group.models';
+import { TargetAnchor } from '../target-anchor';
 
 type CustomGroupExpandedProps = CustomGroupProps &
   CollapsibleGroupProps &
@@ -25,55 +24,6 @@ type CustomGroupExpandedProps = CustomGroupProps &
   WithSelectionProps &
   WithDndDropProps &
   WithContextMenuProps;
-
-class TargetAnchor extends AbstractAnchor {
-  getLocation(reference: Point): Point {
-    return this.closestPointOnRectangle(this.owner.getBounds(), reference);
-  }
-
-  getReferencePoint(): Point {
-    return super.getReferencePoint();
-  }
-
-  private closestPointOnRectangle(rect: Rect, point: Point): Point {
-    // Deconstruct the rectangle and point parameters
-    const { x: rx, y: ry, width, height } = rect;
-    const { x: px, y: py } = point;
-
-    // Calculate the projections on the edges
-    // For left edge
-    const leftX = rx;
-    const leftY = Math.max(ry, Math.min(py, ry + height));
-
-    // For right edge
-    const rightX = rx + width;
-    const rightY = Math.max(ry, Math.min(py, ry + height));
-
-    // For top edge
-    const topX = Math.max(rx, Math.min(px, rx + width));
-    const topY = ry;
-
-    // For bottom edge
-    const bottomX = Math.max(rx, Math.min(px, rx + width));
-    const bottomY = ry + height;
-
-    // Calculate distances to each edge projection
-    const distances = [
-      { x: leftX, y: leftY, dist: Math.hypot(px - leftX, py - leftY) },
-      { x: rightX, y: rightY, dist: Math.hypot(px - rightX, py - rightY) },
-      { x: topX, y: topY, dist: Math.hypot(px - topX, py - topY) },
-      { x: bottomX, y: bottomY, dist: Math.hypot(px - bottomX, py - bottomY) },
-    ];
-
-    // Find the minimum distance
-    const closestPoint = distances.reduce((minPoint, currentPoint) =>
-      currentPoint.dist < minPoint.dist ? currentPoint : minPoint,
-    );
-
-    // Return the closest point
-    return new Point(closestPoint.x, closestPoint.y);
-  }
-}
 
 export const CustomGroupExpanded: FunctionComponent<CustomGroupExpandedProps> = observer(
   ({ className, element, onSelect, label: propsLabel, onContextMenu, onCollapseChange }) => {
