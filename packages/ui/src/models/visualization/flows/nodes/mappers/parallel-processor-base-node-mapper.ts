09a97d9d8f6ea852049eeb6ceeff7c15cf474e96
Ricardo M. <lordrip@gmail.com>
2024-12-03 21:53:10 +0100

    fix(VizNode): Remove addNodeInteraction
    Since the StepToolbar `+` button was removed in favor of using a
    placeholder node or the `arrow` icon, we no longer need to disable the
    node interactions for parallel EIPs like `multicast` and loadBalance`

--- packages/ui/src/models/visualization/flows/nodes/mappers/parallel-processor-base-node-mapper.ts
+++ packages/ui/src/models/visualization/flows/nodes/mappers/parallel-processor-base-node-mapper.ts
@@ -4,7 +4,6 @@ import { IVisualizationNode } from '../../../base-visual-entity';
 import { createVisualizationNode } from '../../../visualization-node';
 import { CamelRouteVisualEntityData, ICamelElementLookupResult } from '../../support/camel-component-types';
 import { BaseNodeMapper } from './base-node-mapper';
-import { CamelComponentSchemaService } from '../../support/camel-component-schema.service';
 
 export abstract class ParallelProcessorBaseNodeMapper extends BaseNodeMapper {
   abstract getProcessorName(): keyof ProcessorDefinition;
@@ -33,31 +32,8 @@ export abstract class ParallelProcessorBaseNodeMapper extends BaseNodeMapper {
        */
       child.setPreviousNode(undefined);
       child.setNextNode(undefined);
-      // adding child node interaction where child cannot append/prepend steps
-      this.addNodeInteraction(child);
     });
 
     return vizNode;
-  }
-
-  addNodeInteraction(vizNode: IVisualizationNode): void {
-    const processorName = (vizNode.data as CamelRouteVisualEntityData).processorName;
-    const stepsProperties = CamelComponentSchemaService.getProcessorStepsProperties(processorName);
-    const canHaveChildren = stepsProperties.find((property) => property.type === 'branch') !== undefined;
-    const canHaveSpecialChildren = Object.keys(stepsProperties).length > 1;
-    const canReplaceStep = CamelComponentSchemaService.canReplaceStep(processorName);
-    const canRemoveStep = !CamelComponentSchemaService.DISABLED_REMOVE_STEPS.includes(processorName);
-    const canBeDisabled = CamelComponentSchemaService.canBeDisabled(processorName);
-
-    vizNode.setNodeInteraction({
-      canHavePreviousStep: false,
-      canHaveNextStep: false,
-      canRemoveFlow: false,
-      canHaveChildren,
-      canHaveSpecialChildren,
-      canReplaceStep,
-      canRemoveStep,
-      canBeDisabled,
-    });
   }
 }
