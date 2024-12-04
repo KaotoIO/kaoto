aeaecb130054669e1923365a722a81a35bd9c4ce
Aurélien Pupier <apupier@redhat.com>
2024-12-04 14:09:21 +0100

    feat: Add 4.9.0 to Catalog for Main, Spring Boot and kamelets
    Signed-off-by: Aurélien Pupier <apupier@redhat.com>

--- packages/catalog-generator/scripts/build-catalogs.mjs
+++ packages/catalog-generator/scripts/build-catalogs.mjs
@@ -14,11 +14,11 @@ const require = createRequire(import.meta.url);
  **/
 const CATALOGS = {
   // https://repo1.maven.org/maven2/org/apache/camel/camel/
-  Main: ['4.8.1', '4.4.4', '4.4.0.redhat-00046', '4.8.0.redhat-00017'],
+  Main: ['4.9.0', '4.8.1', '4.4.4', '4.4.0.redhat-00046', '4.8.0.redhat-00017'],
   // https://repo1.maven.org/maven2/org/apache/camel/quarkus/camel-quarkus/
   Quarkus: ['3.16.0', '3.8.3', '3.8.0.redhat-00018', '3.15.0.redhat-00007'],
   // https://repo1.maven.org/maven2/org/apache/camel/springboot/spring-boot/
-  SpringBoot: ['4.8.1', '4.4.4', '4.4.0.redhat-00039', '4.8.0.redhat-00022'],
+  SpringBoot: ['4.9.0', '4.8.1', '4.4.4', '4.4.0.redhat-00039', '4.8.0.redhat-00022'],
 };
 // https://repo1.maven.org/maven2/org/apache/camel/kamelets/camel-kamelets/
 const KAMELETS_VERSION = '4.8.1';
