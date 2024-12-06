4af3df29568072dc18ede0a6187051cde64db22f
Aurélien Pupier <apupier@redhat.com>
2024-12-06 09:44:53 +0100

    chore: provide more precise urls to search versions of Catalog
    Signed-off-by: Aurélien Pupier <apupier@redhat.com>

--- packages/catalog-generator/scripts/build-catalogs.mjs
+++ packages/catalog-generator/scripts/build-catalogs.mjs
@@ -13,11 +13,14 @@ const require = createRequire(import.meta.url);
  * @type {Record<import('../dist/types').CatalogRuntime, string[]>}
  **/
 const CATALOGS = {
-  // https://repo1.maven.org/maven2/org/apache/camel/camel/
+  // https://repo1.maven.org/maven2/org/apache/camel/camel-catalog/
+  // https://maven.repository.redhat.com/ga/org/apache/camel/camel-catalog/
   Main: ['4.8.1', '4.4.4', '4.4.0.redhat-00046', '4.8.0.redhat-00017'],
-  // https://repo1.maven.org/maven2/org/apache/camel/quarkus/camel-quarkus/
+  // https://repo1.maven.org/maven2/org/apache/camel/quarkus/camel-quarkus-catalog/
+  // https://maven.repository.redhat.com/ga/org/apache/camel/quarkus/camel-quarkus-catalog/
   Quarkus: ['3.16.0', '3.8.3', '3.8.0.redhat-00018', '3.15.0.redhat-00007'],
-  // https://repo1.maven.org/maven2/org/apache/camel/springboot/spring-boot/
+  // https://repo1.maven.org/maven2/org/apache/camel/springboot/camel-catalog-provider-springboot/
+  // https://maven.repository.redhat.com/ga/org/apache/camel/springboot/camel-catalog-provider-springboot/
   SpringBoot: ['4.8.1', '4.4.4', '4.4.0.redhat-00039', '4.8.0.redhat-00022'],
 };
 // https://repo1.maven.org/maven2/org/apache/camel/kamelets/camel-kamelets/
