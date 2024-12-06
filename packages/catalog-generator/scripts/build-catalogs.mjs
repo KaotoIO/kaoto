f3d8c6b73ea85f5c075893f783c530f6b73ea4ec
Aurélien Pupier <apupier@redhat.com>
2024-12-06 09:00:50 +0100

    Align Camel Quarkus catalog 3.8.x (3.8.4) to 4.8.4
    Signed-off-by: Aurélien Pupier <apupier@redhat.com>

--- packages/catalog-generator/scripts/build-catalogs.mjs
+++ packages/catalog-generator/scripts/build-catalogs.mjs
@@ -16,7 +16,7 @@ const CATALOGS = {
   // https://repo1.maven.org/maven2/org/apache/camel/camel/
   Main: ['4.8.1', '4.4.4', '4.4.0.redhat-00046', '4.8.0.redhat-00017'],
   // https://repo1.maven.org/maven2/org/apache/camel/quarkus/camel-quarkus/
-  Quarkus: ['3.16.0', '3.8.3', '3.8.0.redhat-00018', '3.15.0.redhat-00007'],
+  Quarkus: ['3.16.0', '3.8.4', '3.8.0.redhat-00018', '3.15.0.redhat-00007'],
   // https://repo1.maven.org/maven2/org/apache/camel/springboot/spring-boot/
   SpringBoot: ['4.8.1', '4.4.4', '4.4.0.redhat-00039', '4.8.0.redhat-00022'],
 };
