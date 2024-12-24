6497d802a1f8eeb7756fe834ee90887091dd828a
Shivam Gupta <shvmgpt640@gmail.com>
2024-12-24 19:10:41 +0530

    Fix: Navigate to design page on settings save

--- packages/ui/src/components/Settings/SettingsForm.test.tsx
+++ packages/ui/src/components/Settings/SettingsForm.test.tsx
@@ -1,62 +1,50 @@
-import { act, fireEvent, render } from '@testing-library/react';
+import { act, fireEvent, render, screen } from '@testing-library/react';
 import { AbstractSettingsAdapter, DefaultSettingsAdapter } from '../../models/settings';
 import { ReloadContext, SettingsProvider } from '../../providers';
 import { SettingsForm } from './SettingsForm';
+import { MemoryRouter } from 'react-router-dom';
 
 describe('SettingsForm', () => {
   let reloadPage: jest.Mock;
   let settingsAdapter: AbstractSettingsAdapter;
 
+  const wrapper = ({ children }: { children: React.ReactNode }) => {
+    return (
+      <MemoryRouter>
+        <ReloadContext.Provider value={{ reloadPage, lastRender: 0 }}>
+          <SettingsProvider adapter={settingsAdapter}>{children}</SettingsProvider>
+        </ReloadContext.Provider>
+      </MemoryRouter>
+    );
+  };
+
   beforeEach(() => {
     reloadPage = jest.fn();
     settingsAdapter = new DefaultSettingsAdapter();
+    render(<SettingsForm />, { wrapper });
   });
 
   it('should render', () => {
-    const wrapper = render(
-      <ReloadContext.Provider value={{ reloadPage, lastRender: 0 }}>
-        <SettingsProvider adapter={settingsAdapter}>
-          <SettingsForm />
-        </SettingsProvider>
-      </ReloadContext.Provider>,
-    );
-
-    expect(wrapper.getByTestId('settings-form')).toMatchSnapshot();
+    expect(screen.getByTestId('settings-form')).toMatchSnapshot();
   });
 
   it('should update settings upon clicking save', () => {
-    const wrapper = render(
-      <ReloadContext.Provider value={{ reloadPage, lastRender: 0 }}>
-        <SettingsProvider adapter={settingsAdapter}>
-          <SettingsForm />
-        </SettingsProvider>
-      </ReloadContext.Provider>,
-    );
-
     act(() => {
-      const input = wrapper.getByLabelText('Camel Catalog URL');
+      const input = screen.getByLabelText('Camel Catalog URL');
       fireEvent.change(input, { target: { value: 'http://localhost:8080' } });
     });
 
     act(() => {
-      const button = wrapper.getByTestId('settings-form-save-btn');
+      const button = screen.getByTestId('settings-form-save-btn');
       fireEvent.click(button);
     });
 
     expect(settingsAdapter.getSettings().catalogUrl).toBe('http://localhost:8080');
   });
 
-  it('should not update settings if the save button was clicked', () => {
-    const wrapper = render(
-      <ReloadContext.Provider value={{ reloadPage, lastRender: 0 }}>
-        <SettingsProvider adapter={settingsAdapter}>
-          <SettingsForm />
-        </SettingsProvider>
-      </ReloadContext.Provider>,
-    );
-
+  it('should not update settings if the save button was not clicked', () => {
     act(() => {
-      const input = wrapper.getByLabelText('Camel Catalog URL');
+      const input = screen.getByLabelText('Camel Catalog URL');
       fireEvent.change(input, { target: { value: 'http://localhost:8080' } });
     });
 
@@ -64,16 +52,8 @@ describe('SettingsForm', () => {
   });
 
   it('should reload the page upon clicking save', () => {
-    const wrapper = render(
-      <ReloadContext.Provider value={{ reloadPage, lastRender: 0 }}>
-        <SettingsProvider adapter={settingsAdapter}>
-          <SettingsForm />
-        </SettingsProvider>
-      </ReloadContext.Provider>,
-    );
-
     act(() => {
-      const button = wrapper.getByTestId('settings-form-save-btn');
+      const button = screen.getByTestId('settings-form-save-btn');
       fireEvent.click(button);
     });
 
