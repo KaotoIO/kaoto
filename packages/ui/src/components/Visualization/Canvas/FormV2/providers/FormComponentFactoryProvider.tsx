c4a47bd5ecc1cd857f75d9fff36c09be1fd5a6b8
Ricardo M. <lordrip@gmail.com>
2025-01-15 21:03:23 +0100

    feat(Form): Support anyOf & oneOf
    This commit adds support for anyOf & oneOf so it better reflects the
    Caml YAML DSL schema.
    
    fix: https://github.com/KaotoIO/kaoto/issues/1934
    fix: https://github.com/KaotoIO/kaoto/issues/1913
    fix: https://github.com/KaotoIO/kaoto/issues/794

---
+++ packages/ui/src/components/Visualization/Canvas/FormV2/providers/FormComponentFactoryProvider.tsx
@@ -0,0 +1,39 @@
+import { createContext, FunctionComponent, PropsWithChildren, useCallback } from 'react';
+import { KaotoSchemaDefinition } from '../../../../../models';
+import { BooleanField } from '../fields/BooleanField';
+import { DisabledField } from '../fields/DisabledField';
+import { ObjectField } from '../fields/ObjectField/ObjectField';
+import { OneOfField } from '../fields/OneOfField/OneOfField';
+import { StringField } from '../fields/StringField';
+import { FieldProps } from '../typings';
+
+type FormComponentFactoryContextValue = (schema: KaotoSchemaDefinition['schema']) => FunctionComponent<FieldProps>;
+
+export const FormComponentFactoryContext = createContext<FormComponentFactoryContextValue | undefined>(undefined);
+
+export const FormComponentFactoryProvider: FunctionComponent<PropsWithChildren> = ({ children }) => {
+  const factory = useCallback<FormComponentFactoryContextValue>((schema) => {
+    switch (schema.type) {
+      case 'string':
+      case 'number':
+      case 'integer':
+        return StringField;
+      case 'boolean':
+        return BooleanField;
+      case 'object':
+        return ObjectField;
+      case 'array':
+        return () => <div>Array field</div>;
+    }
+
+    if (Array.isArray(schema.oneOf)) {
+      return OneOfField;
+    } else if (Array.isArray(schema.anyOf)) {
+      return () => <div>AnyOf field</div>;
+    }
+
+    return DisabledField;
+  }, []);
+
+  return <FormComponentFactoryContext.Provider value={factory}>{children}</FormComponentFactoryContext.Provider>;
+};
