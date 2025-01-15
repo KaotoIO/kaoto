c6699704747f3a389c7946f1478dfa68caba1930
Ricardo M. <lordrip@gmail.com>
2025-01-15 12:50:46 +0100

    Feat: Added new EIPGenerator

---
+++ packages/catalog-generator/src/main/java/io/kaoto/camelcatalog/generators/EIPGenerator.java
@@ -0,0 +1,68 @@
+package io.kaoto.camelcatalog.generators;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.SerializationFeature;
+import com.fasterxml.jackson.databind.node.ObjectNode;
+import org.apache.camel.catalog.CamelCatalog;
+
+import java.util.ArrayList;
+import java.util.LinkedHashMap;
+import java.util.List;
+import java.util.Map;
+
+public class EIPGenerator {
+    CamelCatalog camelCatalog;
+    String camelYamlSchema;
+    ObjectMapper jsonMapper = new ObjectMapper()
+            .configure(SerializationFeature.ORDER_MAP_ENTRIES_BY_KEYS, true);
+    ObjectNode camelYamlSchemaNode;
+    CamelYAMLSchemaReader camelYAMLSchemaReader;
+
+    public EIPGenerator(CamelCatalog camelCatalog, String camelYamlSchema) throws JsonProcessingException {
+        this.camelCatalog = camelCatalog;
+        this.camelYamlSchema = camelYamlSchema;
+        this.camelYamlSchemaNode = (ObjectNode) jsonMapper.readTree(camelYamlSchema);
+        this.camelYAMLSchemaReader = new CamelYAMLSchemaReader(camelYamlSchemaNode);
+    }
+
+    public Map<String, ObjectNode> generate() {
+        Map<String, ObjectNode> eipMap = new LinkedHashMap<>();
+
+        getEIPNames().forEach(eipName -> {
+            var eipJSON = getEIPJson(eipName);
+            var eipJSONSchema = camelYAMLSchemaReader.getJSONSchema(eipName);
+
+            eipJSON.set("propertiesSchema", eipJSONSchema);
+
+            eipMap.put(eipName, eipJSON);
+        });
+
+        return eipMap;
+    }
+
+    List<String> getEIPNames() {
+        var iterator = this.camelYamlSchemaNode.get("items").get("definitions")
+                .get("org.apache.camel.model.ProcessorDefinition")
+                .get("properties")
+                .fields();
+
+        List<String> eipNames = new ArrayList<>();
+        while (iterator.hasNext()) {
+            var entry = iterator.next();
+            eipNames.add(entry.getKey());
+        }
+
+        return eipNames;
+    }
+
+    ObjectNode getEIPJson(String eipName) {
+        String eipJson = camelCatalog.modelJSonSchema(eipName);
+
+        try {
+            return (ObjectNode) jsonMapper.readTree(eipJson);
+        } catch (JsonProcessingException e) {
+            throw new RuntimeException(String.format("Cannot load %s JSON model", eipName), e);
+        }
+    }
+}
