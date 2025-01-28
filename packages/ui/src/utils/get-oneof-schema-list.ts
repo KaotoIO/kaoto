3e2df12d561ba4be9d290a5cf9971d90ce3558d1
Ricardo M. <lordrip@gmail.com>
2025-01-28 10:56:50 +0100

    chore: Improve single property schema naming

--- packages/ui/src/utils/get-oneof-schema-list.ts
+++ packages/ui/src/utils/get-oneof-schema-list.ts
@@ -1,4 +1,5 @@
 import { KaotoSchemaDefinition } from '../models';
+import { camelCaseToSpaces } from './camel-case-to-space';
 import { getResolvedSchema } from './get-resolved-schema';
 import { isDefined } from './is-defined';
 import { resolveSchemaWithRef } from './resolve-ref-if-needed';
@@ -44,29 +45,36 @@ export const getOneOfSchemaListV2 = (
   oneOfList: KaotoSchemaDefinition['schema'][],
   definitions: KaotoSchemaDefinition['schema']['definitions'] = {},
 ): OneOfSchemas[] => {
-  return (
-    oneOfList
-      /** Ignore the `not` schemas */
-      .filter((oneOfSchema) => !isDefined(oneOfSchema.not))
-      .map((schema, index) => {
-        const resolvedSchema = resolveSchemaWithRef(schema, definitions);
+  const list = oneOfList
+    /** Ignore the `not` schemas */
+    .filter((oneOfSchema) => !isDefined(oneOfSchema.not))
+    .map((schema, index) => {
+      const resolvedSchema = resolveSchemaWithRef(schema, definitions);
 
-        let name = resolvedSchema.title;
-        let description = resolvedSchema.description;
+      let name = resolvedSchema.title;
+      let description = resolvedSchema.description;
 
-        const schemaProps = Object.keys(resolvedSchema.properties ?? {});
-        const isSinglePropertySchema = schemaProps.length === 1;
-        if (isSinglePropertySchema && !name) {
-          name = resolvedSchema.properties![schemaProps[0]].title ?? schemaProps[0];
-        }
-        if (isSinglePropertySchema && !description) {
-          description = resolvedSchema.properties![schemaProps[0]].description;
-        }
+      const schemaProps = Object.keys(resolvedSchema.properties ?? {});
+      const isSinglePropertySchema = schemaProps.length === 1;
+      if (isSinglePropertySchema) {
+        const singlePropertySchema = resolveSchemaWithRef(resolvedSchema.properties![schemaProps[0]], definitions);
+        name ??= singlePropertySchema.title ?? camelCaseToSpaces(schemaProps[0], { capitalize: true });
+        description ??= singlePropertySchema.description;
+      }
 
-        name = name ?? `Schema ${index}`;
-        description = description ?? resolvedSchema.description;
+      name ??= `Schema ${index}`;
 
-        return { name, description, schema: resolvedSchema };
-      })
-  );
+      return { name, description, schema: resolvedSchema };
+    });
+
+  /** Handling cases where an EIP, Language or Dataformat could be represented in 2 different ways  */
+  if (list.length === 2) {
+    const [firstSchema, secondSchema] = list;
+    if (firstSchema.schema.type === 'string' && secondSchema.schema.type === 'object') {
+      firstSchema.name = 'Simple';
+      secondSchema.name = 'Advanced';
+    }
+  }
+
+  return list;
 };
