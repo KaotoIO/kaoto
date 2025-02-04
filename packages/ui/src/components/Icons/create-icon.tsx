10b13734df4789d24b86943942bf654e5a7d74c7
Ricardo M. <lordrip@gmail.com>
2025-02-04 17:39:53 +0100

    fix(Canvas): Replace png icons with svg
    fix: https://github.com/KaotoIO/kaoto/issues/1948

---
+++ packages/ui/src/components/Icons/create-icon.tsx
@@ -0,0 +1,64 @@
+import { Component, ComponentClass, HTMLProps, SVGProps } from 'react';
+
+export interface IconDefinition {
+  name?: string;
+  width: number;
+  height: number;
+  svgPath: string;
+  xOffset?: number;
+  yOffset?: number;
+  transform?: string;
+}
+
+export interface SVGIconProps extends Omit<HTMLProps<SVGElement>, 'ref'> {
+  title?: string;
+  className?: string;
+}
+
+let currentId = 0;
+
+/**
+ * Copied from @patternfly/react-icons
+ * Factory to create Icon class components for consumers
+ */
+export function createIcon({
+  name,
+  xOffset = 0,
+  yOffset = 0,
+  transform,
+  width,
+  height,
+  svgPath,
+}: IconDefinition): ComponentClass<SVGIconProps> {
+  return class SVGIcon extends Component<SVGIconProps> {
+    static displayName = name;
+
+    id = `icon-title-${currentId++}`;
+
+    render() {
+      const { title, className, ...props } = this.props;
+      const classes = className ? `pf-v6-svg ${className}` : 'pf-v6-svg';
+
+      const hasTitle = Boolean(title);
+      const viewBox = [xOffset, yOffset, width, height].join(' ');
+
+      return (
+        <svg
+          className={classes}
+          viewBox={viewBox}
+          fill="currentColor"
+          aria-labelledby={hasTitle ? this.id : null}
+          aria-hidden={hasTitle ? null : true}
+          transform={transform}
+          role="img"
+          width="1em"
+          height="1em"
+          {...(props as Omit<SVGProps<SVGElement>, 'ref'>)} // Lie.
+        >
+          {hasTitle && <title id={this.id}>{title}</title>}
+          <path d={svgPath} />
+        </svg>
+      );
+    }
+  };
+}
