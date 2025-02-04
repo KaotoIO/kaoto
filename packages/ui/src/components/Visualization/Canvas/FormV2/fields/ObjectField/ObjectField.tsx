5b4da00acc895b4a771c29563cab2ed59e89ac1d
Ricardo M. <lordrip@gmail.com>
2025-02-04 12:09:13 +0100

    feat(Form): Generate object fields lazily
    fix: https://github.com/KaotoIO/kaoto/issues/1972

--- packages/ui/src/components/Visualization/Canvas/FormV2/fields/ObjectField/ObjectField.tsx
+++ packages/ui/src/components/Visualization/Canvas/FormV2/fields/ObjectField/ObjectField.tsx
@@ -1,36 +1,36 @@
 import { Button } from '@patternfly/react-core';
-import { TrashIcon } from '@patternfly/react-icons';
-import { FunctionComponent, useContext, useMemo } from 'react';
+import { PencilAltIcon, TrashIcon } from '@patternfly/react-icons';
+import { FunctionComponent, useContext, useState } from 'react';
 import { isDefined, ROOT_PATH } from '../../../../../../utils';
+import { useFieldValue } from '../../hooks/field-value';
 import { SchemaContext } from '../../providers/SchemaProvider';
 import { FieldProps } from '../../typings';
 import { FieldWrapper } from '../FieldWrapper';
 import { ObjectFieldGrouping } from './ObjectFieldGrouping';
 
-export const ObjectField: FunctionComponent<FieldProps> = ({ propName, onRemove }) => {
+export const ObjectField: FunctionComponent<FieldProps> = ({ propName, onRemove: onRemoveProps }) => {
   const { schema } = useContext(SchemaContext);
+  const { value, onChange } = useFieldValue<object>(propName);
+  const [isExpanded, setIsExpanded] = useState(isDefined(value));
+
   if (!isDefined(schema)) {
     throw new Error(`ObjectField: schema is not defined for ${propName}`);
   }
 
-  const actions = useMemo(
-    () => (
-      <>
-        {onRemove && (
-          <Button
-            variant="plain"
-            aria-label="Remove object"
-            title="Remove object"
-            onClick={() => {
-              onRemove(propName);
-            }}
-            icon={<TrashIcon />}
-          />
-        )}
-      </>
-    ),
-    [onRemove, propName],
-  );
+  const onSet = () => {
+    setIsExpanded(true);
+  };
+
+  const onRemove = () => {
+    if (isDefined(onRemoveProps)) {
+      onRemoveProps(propName);
+      return;
+    }
+
+    /** Clear field by removing its value */
+    onChange(undefined as unknown as object);
+    setIsExpanded(false);
+  };
 
   if (propName === ROOT_PATH || !schema.title) {
     return <ObjectFieldGrouping propName={propName} />;
@@ -43,9 +43,31 @@ export const ObjectField: FunctionComponent<FieldProps> = ({ propName, onRemove 
       title={schema.title}
       description={schema.description}
       defaultValue={schema.default}
-      actions={actions}
+      actions={
+        <>
+          {!isExpanded && (
+            <Button
+              variant="plain"
+              aria-label="Set object"
+              title="Set object"
+              onClick={onSet}
+              icon={<PencilAltIcon />}
+            />
+          )}
+
+          {isExpanded && (
+            <Button
+              variant="plain"
+              aria-label="Remove object"
+              title="Remove object"
+              onClick={onRemove}
+              icon={<TrashIcon />}
+            />
+          )}
+        </>
+      }
     >
-      <ObjectFieldGrouping propName={propName} />
+      {isExpanded && <ObjectFieldGrouping propName={propName} />}
     </FieldWrapper>
   );
 };
