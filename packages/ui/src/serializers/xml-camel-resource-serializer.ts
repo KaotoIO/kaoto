4609e7b052c7224cc080d49dbc2c5c93fb801960
mmelko <mmelko@redhat.com>
2025-02-13 18:36:07 +0100

    feat(xml-support): Use XML parsers and serializers to allow using XML with Kaoto
    (cherry picked from commit 7b03dd850873f466192bc80f70836f9bde22f0f3)

--- packages/ui/src/serializers/xml-camel-resource-serializer.ts
+++ packages/ui/src/serializers/xml-camel-resource-serializer.ts
@@ -1,27 +1,69 @@
-import { CamelResource } from '../models/camel';
+import { isXML, KaotoXmlParser } from './xml/kaoto-xml-parser';
+import { CamelResource } from '../models/camel/camel-resource';
+import { EntityDefinition, KaotoXmlSerializer } from './xml/serializers/kaoto-xml-serializer';
+import { formatXml } from './xml/xml-utils';
 import { CamelResourceSerializer } from './camel-resource-serializer';
 import { CamelYamlDsl, Integration, Kamelet, KameletBinding, Pipe } from '@kaoto/camel-catalog/types';
+import { EntityType } from '../models/camel/entities';
 
 export class XmlCamelResourceSerializer implements CamelResourceSerializer {
-  static isApplicable(_code: unknown): boolean {
-    return false;
+  private comments: string[] = [];
+
+  getLabel(): string {
+    return 'XML';
   }
 
-  parse(_code: string): CamelYamlDsl | Integration | Kamelet | KameletBinding | Pipe {
-    //TODO implement
-    return {};
+  static isApplicable(code: unknown): boolean {
+    return isXML(code as string);
   }
 
-  serialize(_resource: CamelResource): string {
-    //TODO implement
-    return '';
+  xmlSerializer: XMLSerializer = new XMLSerializer();
+
+  parse(code: unknown): CamelYamlDsl | Integration | Kamelet | KameletBinding | Pipe {
+    const xmlParser = new KaotoXmlParser();
+    const entities = xmlParser.parseXML(code as string);
+    this.extractComments(code as string);
+    return entities as CamelYamlDsl;
+  }
+
+  serialize(resource: CamelResource): string {
+    const entities: EntityDefinition[] = resource
+      .getEntities()
+      .filter((entity) => entity.type === EntityType.Beans) as EntityDefinition[];
+    entities.push(...(resource.getVisualEntities() as EntityDefinition[]));
+
+    const xmlDocument = KaotoXmlSerializer.serialize(entities);
+    let xmlString = this.xmlSerializer.serializeToString(xmlDocument);
+
+    xmlString = this.insertComments(xmlString);
+
+    return formatXml(xmlString);
   }
 
   getComments(): string[] {
-    return [];
+    return this.comments;
   }
 
-  setComments(_comments: string[]): void {
-    //TODO implement
+  setComments(comments: string[]): void {
+    this.comments = comments;
+  }
+
+  private extractComments(xml: string): void {
+    const commentRegex = /<!--([\s\S]*?)-->/g;
+    this.comments = [];
+    let match;
+
+    while ((match = commentRegex.exec(xml)) !== null) {
+      if (xml.slice(0, match.index).trim() === '') {
+        this.comments.push(match[1].trim());
+      } else {
+        break;
+      }
+    }
+  }
+
+  private insertComments(xml: string): string {
+    const commentsString = this.comments.map((comment) => `<!-- ${comment} -->`).join('\n');
+    return commentsString ? commentsString + '\n' + xml : xml;
   }
 }
