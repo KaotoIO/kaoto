c1f11df9b9e2a08eeedcd2a90848ccc12f5bb502
Shivam Gupta <shivamgu@shivamgu-thinkpadp16vgen1.bengluru.csb>
2025-02-14 19:04:56 +0530

    Fix(CanvasForm): Added Fix for Required and Modified Fields

--- packages/ui/src/components/Visualization/Canvas/FormV2/fields/ObjectField/ObjectFieldGrouping.tsx
+++ packages/ui/src/components/Visualization/Canvas/FormV2/fields/ObjectField/ObjectFieldGrouping.tsx
@@ -1,17 +1,17 @@
-import { FormFieldGroupExpandable, FormFieldGroupHeader } from '@patternfly/react-core';
 import { FunctionComponent, useContext, useMemo } from 'react';
 import { FilteredFieldContext } from '../../../../../../providers';
 import { getFieldGroupsV2, getFilteredProperties, isDefined } from '../../../../../../utils';
-import { capitalizeString } from '../../../../../../utils/capitalize-string';
 import { SchemaContext, SchemaProvider } from '../../providers/SchemaProvider';
 import { FieldProps } from '../../typings';
 import { AnyOfField } from './AnyOfField';
 import { ObjectFieldInner } from './ObjectFieldInner';
+import { GroupFields } from './GroupFields';
 
 const SPACE_REGEX = /\s/g;
 
 export const ObjectFieldGrouping: FunctionComponent<FieldProps> = ({ propName }) => {
   const { schema } = useContext(SchemaContext);
+
   if (!isDefined(schema)) {
     throw new Error(`ObjectFieldGrouping: schema is not defined for ${propName}`);
   }
@@ -37,21 +37,7 @@ export const ObjectFieldGrouping: FunctionComponent<FieldProps> = ({ propName })
       {Array.isArray(schema.anyOf) && <AnyOfField propName={propName} anyOf={schema.anyOf} />}
 
       {/* Grouped properties */}
-      {groupedProperties.groups.map(([groupName, groupProperties]) => {
-        const name = capitalizeString(groupName);
-
-        return (
-          <FormFieldGroupExpandable
-            key={`${name}-section-toggle`}
-            toggleAriaLabel={`Toggle ${name} group`}
-            header={<FormFieldGroupHeader titleText={{ text: name, id: `${propName}-${name}` }} />}
-          >
-            <SchemaProvider schema={{ properties: groupProperties }}>
-              <ObjectFieldInner propName={propName} requiredProperties={requiredProperties} />
-            </SchemaProvider>
-          </FormFieldGroupExpandable>
-        );
-      })}
+      <GroupFields propName={propName} groups={groupedProperties.groups} requiredProperties={requiredProperties} />
     </>
   );
 };
