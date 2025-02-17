cdabc62cbca6cceb6ec75194c151396b311bf0e3
mmelko <mmelko@redhat.com>
2025-02-17 21:04:18 +0100

    refactor: Apply code review feedback
    (cherry picked from commit 279cbdf25063feb9f64a9a5e4866fc1dd3246b86)

--- packages/ui/src/serializers/xml-to-yaml.test.ts
+++ packages/ui/src/serializers/xml-to-yaml.test.ts
@@ -10,34 +10,26 @@ import { KaotoXmlParser } from './xml/kaoto-xml-parser';
 describe('XmlParser - XML to YAML comparison', () => {
   let parser: KaotoXmlParser;
   let yamlSerializer: YamlCamelResourceSerializer;
+  const xmlDir = path.join(__dirname, '../stubs/xml');
+  const yamlDir = path.join(__dirname, '../stubs/yaml');
+  const xmlFiles = fs.readdirSync(xmlDir).filter((file) => file.endsWith('.xml'));
 
   beforeAll(async () => {
     parser = new KaotoXmlParser();
-    yamlSerializer = new YamlCamelResourceSerializer(); // Initialize your YAML serializer
+    yamlSerializer = new YamlCamelResourceSerializer();
     const catalogsMap = await getFirstCatalogMap(catalogLibrary as CatalogLibrary);
     CamelCatalogService.setCatalogKey(CatalogKind.Processor, catalogsMap.modelCatalogMap);
   });
 
-  const xmlDir = path.join(__dirname, '../stubs/xml');
-  const yamlDir = path.join(__dirname, '../stubs/yaml');
-
-  const xmlFiles = fs.readdirSync(xmlDir).filter((file) => file.endsWith('.xml'));
-
-  const tesXmlToYaml = (xmlFile: string) => {
+  it.each(xmlFiles)('parses and compares %s correctly', (xmlFile) => {
     const xmlFilePath = path.join(xmlDir, xmlFile);
     const yamlFilePath = path.join(yamlDir, xmlFile.replace('.xml', '.yaml'));
 
     const xmlContent = fs.readFileSync(xmlFilePath, 'utf-8');
     const expectedYamlContent = fs.readFileSync(yamlFilePath, 'utf-8');
-    const expectedYaml = yamlSerializer.parse(expectedYamlContent); // Use your YAML serializer
+    const expectedYaml = yamlSerializer.parse(expectedYamlContent);
     const result = parser.parseXML(xmlContent);
 
     expect(result).toEqual(expectedYaml);
-  };
-
-  xmlFiles.forEach((xmlFile) => {
-    it(`parses and compares ${xmlFile} correctly`, () => {
-      tesXmlToYaml(xmlFile);
-    });
   });
 });
