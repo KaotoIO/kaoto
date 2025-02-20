d7549ab1c512299f76d69ee7f3b0d2e9b44f6da2
mmelko <mmelko@redhat.com>
2025-02-20 11:42:41 +0100

    refactor(ContextToolbar): Move SerializerSelector out of ContextToolbar and add it as a additionalControls component
    fix:add missing key to SerializerSelector

--- packages/ui/src/pages/Design/DesignPage.tsx
+++ packages/ui/src/pages/Design/DesignPage.tsx
@@ -1,18 +1,25 @@
-import { FunctionComponent, ReactNode, useContext } from 'react';
+import { FunctionComponent, JSX, ReactNode, useContext } from 'react';
 import { Visualization } from '../../components/Visualization';
 import { CatalogModalProvider } from '../../providers/catalog-modal.provider';
 import { ActionConfirmationModalContextProvider } from '../../providers/action-confirmation-modal.provider';
 import { EntitiesContext } from '../../providers/entities.provider';
 import './DesignPage.scss';
 
-export const DesignPage: FunctionComponent<{ fallback?: ReactNode }> = (props) => {
+export const DesignPage: FunctionComponent<{ fallback?: ReactNode; additionalToolbarControls?: JSX.Element[] }> = (
+  props,
+) => {
   const entitiesContext = useContext(EntitiesContext);
   const visualEntities = entitiesContext?.visualEntities ?? [];
 
   return (
     <CatalogModalProvider>
       <ActionConfirmationModalContextProvider>
-        <Visualization className="canvas-page" entities={visualEntities} fallback={props.fallback} />
+        <Visualization
+          className="canvas-page"
+          entities={visualEntities}
+          fallback={props.fallback}
+          additionalToolbarControls={props.additionalToolbarControls}
+        />
       </ActionConfirmationModalContextProvider>
     </CatalogModalProvider>
   );
