3297c43c9461f3c7ded247b9d095620f2c710105
mmelko <mmelko@redhat.com>
2025-02-26 12:19:32 +0100

    feat(xml-support): Hide DSL selector in VS-Code when XML is used

--- packages/ui/src/components/Visualization/ContextToolbar/ContextToolbar.tsx
+++ packages/ui/src/components/Visualization/ContextToolbar/ContextToolbar.tsx
@@ -3,7 +3,6 @@ import { FunctionComponent, JSX, useContext } from 'react';
 import { sourceSchemaConfig } from '../../../models/camel';
 import { EntitiesContext } from '../../../providers/entities.provider';
 import './ContextToolbar.scss';
-import { DSLSelector } from './DSLSelector/DSLSelector';
 import { FlowClipboard } from './FlowClipboard/FlowClipboard';
 import { FlowExportImage } from './FlowExportImage/FlowExportImage';
 import { FlowsMenu } from './Flows/FlowsMenu';
@@ -15,9 +14,6 @@ export const ContextToolbar: FunctionComponent<{ additionalControls?: JSX.Elemen
   const isMultipleRoutes = sourceSchemaConfig.config[currentSchemaType].multipleRoute;
 
   const toolbarItems: JSX.Element[] = [
-    <ToolbarItem key="toolbar-dsl-selector">
-      <DSLSelector />
-    </ToolbarItem>,
     <ToolbarItem key="toolbar-flows-list">
       <FlowsMenu />
     </ToolbarItem>,
