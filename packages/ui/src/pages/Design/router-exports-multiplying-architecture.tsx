120b2f1d20ce72c9d2119617e5d4363eadf9e4d9
mmelko <mmelko@redhat.com>
2025-02-26 12:19:32 +0100

    feat(xml-support): Hide DSL selector in VS-Code when XML is used

--- packages/ui/src/pages/Design/router-exports-multiplying-architecture.tsx
+++ packages/ui/src/pages/Design/router-exports-multiplying-architecture.tsx
@@ -1,3 +1,22 @@
 import { DesignPage } from './DesignPage';
+import { FunctionComponent, useContext } from 'react';
+import { EntitiesContext } from '../../providers';
+import { SerializerType } from '../../serializers';
+import { ToolbarItem } from '@patternfly/react-core';
+import { DSLSelector } from '../../components/Visualization/ContextToolbar/DSLSelector/DSLSelector';
 
-export const element = <DesignPage />;
+const DesignPageWrapper: FunctionComponent = () => {
+  const { camelResource } = useContext(EntitiesContext)!;
+
+  const additionalControls =
+    camelResource.getSerializerType() === SerializerType.XML
+      ? []
+      : [
+          <ToolbarItem key="toolbar-dsl-selector">
+            <DSLSelector />
+          </ToolbarItem>,
+        ];
+  return <DesignPage additionalToolbarControls={additionalControls} />;
+};
+
+export const element = <DesignPageWrapper />;
