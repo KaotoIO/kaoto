291a4950abf5519da2ad629657a8d7e2d316a236
mmelko <mmelko@redhat.com>
2025-03-04 15:31:36 +0100

    feat(ContextToolbar): Add Serializer selector to allow choose target format
    (cherry picked from commit 8971aa5a3d0579bd3496df8bc85512b9c8393c28)

--- packages/ui/src/components/Visualization/ContextToolbar/ContextToolbar.tsx
+++ packages/ui/src/components/Visualization/ContextToolbar/ContextToolbar.tsx
@@ -1,6 +1,6 @@
 import { Toolbar, ToolbarContent, ToolbarItem } from '@patternfly/react-core';
 import { FunctionComponent, useContext } from 'react';
-import { sourceSchemaConfig } from '../../../models/camel';
+import { sourceSchemaConfig, SourceSchemaType } from '../../../models/camel';
 import { EntitiesContext } from '../../../providers/entities.provider';
 import './ContextToolbar.scss';
 import { DSLSelector } from './DSLSelector/DSLSelector';
@@ -10,6 +10,7 @@ import { FlowExportImage } from './FlowExportImage/FlowExportImage';
 import { FlowsMenu } from './Flows/FlowsMenu';
 import { NewEntity } from './NewEntity/NewEntity';
 import { RuntimeSelector } from './RuntimeSelector/RuntimeSelector';
+import { SerializerSelector } from './SerializerSelector/SerializerSelector';
 
 export const ContextToolbar: FunctionComponent = () => {
   const { currentSchemaType } = useContext(EntitiesContext)!;
@@ -23,6 +24,13 @@ export const ContextToolbar: FunctionComponent = () => {
       <FlowsMenu />
     </ToolbarItem>,
   ];
+  if (currentSchemaType === SourceSchemaType.Route) {
+    toolbarItems.unshift(
+      <ToolbarItem key="toolbar-serializer-selector">
+        <SerializerSelector />
+      </ToolbarItem>,
+    );
+  }
 
   if (isMultipleRoutes) {
     toolbarItems.push(
@@ -45,6 +53,7 @@ export const ContextToolbar: FunctionComponent = () => {
           <ToolbarItem key="toolbar-export-document">
             <ExportDocument />
           </ToolbarItem>,
+
           <RuntimeSelector key="runtime-selector" />,
         ])}
       </ToolbarContent>
