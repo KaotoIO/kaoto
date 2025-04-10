dcc5f5433f8ce818cbe3d4722f8546b8c35d5bc5
Tomas Plevko <tplevko@redhat.com>
2025-04-10 15:06:53 +0200

    storybook: Add storybook pages for PipeErrorHandler and Metadata

---
+++ packages/ui-tests/stories/kaotoForm/PipeErrorHandler.stories.tsx
@@ -0,0 +1,47 @@
+import { KaotoForm, KaotoFormProps, PipeErrorHandlerPage } from '@kaoto/kaoto';
+import { Meta, StoryFn } from '@storybook/react';
+import { fn } from '@storybook/test';
+
+import {
+  CanvasFormTabsContext,
+  CanvasFormTabsContextResult,
+  EntitiesContext,
+  KaotoSchemaDefinition,
+  PipeResource,
+} from '@kaoto/kaoto/testing';
+import errorHandler from '../../cypress/fixtures/metadata/errorHandlerSchema.json';
+
+export default {
+  title: 'KaotoForm/PipeErrorHandler',
+  component: PipeErrorHandlerPage,
+} as Meta<typeof PipeErrorHandlerPage>;
+const formTabsValue: CanvasFormTabsContextResult = {} as CanvasFormTabsContextResult;
+const getErrorHandlerModel = () => {};
+const onChangeModel = () => {};
+
+const camelResource = new PipeResource();
+const mockEntitiesContext = {
+  camelResource,
+  entities: camelResource.getEntities(),
+  visualEntities: camelResource.getVisualEntities(),
+  currentSchemaType: camelResource.getType(),
+  updateSourceCodeFromEntities: fn(),
+  updateEntitiesFromCamelResource: fn(),
+};
+
+const Template: StoryFn<typeof KaotoForm> = () => {
+  return (
+    <EntitiesContext.Provider value={mockEntitiesContext}>
+      <CanvasFormTabsContext.Provider value={formTabsValue}>
+        <KaotoForm
+          data-testid="pipe-error-handler-form"
+          schema={errorHandler as unknown as KaotoSchemaDefinition['schema']}
+          model={getErrorHandlerModel()}
+          onChange={onChangeModel as KaotoFormProps['onChange']}
+        />
+      </CanvasFormTabsContext.Provider>
+    </EntitiesContext.Provider>
+  );
+};
+
+export const PipeErrorHandler = Template.bind({});
