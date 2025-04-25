bdd7041efa27bd17417ebf0f9b44cab15380a91e
mmelko <mmelko@redhat.com>
2025-04-25 17:15:40 +0200

    feat(init-visible-flows): retain existing visibility state

--- packages/ui/src/utils/init-visible-flows.ts
+++ packages/ui/src/utils/init-visible-flows.ts
@@ -1,8 +1,18 @@
 import { IVisibleFlows } from '../models/visualization/flows/support/flows-visibility';
 
-export const initVisibleFlows = (flowsIds: string[]) => {
-  return flowsIds.reduce((flows, id, index) => {
-    flows[id] = index === 0;
+const ensureAtLeastOneVisibleFlow = (state: IVisibleFlows) => {
+  const entries = Object.keys(state);
+  if (entries.length > 0 && Object.values(state).every((visible) => !visible)) {
+    state[entries[0]] = true;
+  }
+  return state;
+};
+
+export const initVisibleFlows = (flowsIds: string[], state: IVisibleFlows = {}): IVisibleFlows => {
+  const visibleFlows = flowsIds.reduce((flows, id) => {
+    flows[id] = state[id] ?? false;
     return flows;
   }, {} as IVisibleFlows);
+
+  return ensureAtLeastOneVisibleFlow(visibleFlows);
 };
