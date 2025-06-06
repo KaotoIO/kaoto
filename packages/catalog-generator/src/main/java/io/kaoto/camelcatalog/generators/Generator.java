cc5c852c6be858dfc7f3ae5e47b5907f4733894e
mmelko <mmelko@redhat.com>
2025-06-06 11:18:56 +0200

    feat: Move the catalog-generator to standalone npm package

--- packages/catalog-generator/src/main/java/io/kaoto/camelcatalog/generators/Generator.java
+++
@@ -1,9 +0,0 @@
-package io.kaoto.camelcatalog.generators;
-
-import com.fasterxml.jackson.databind.node.ObjectNode;
-
-import java.util.Map;
-
-public interface Generator {
-    Map<String, ObjectNode> generate();
-}
