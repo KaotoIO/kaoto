1e37f6277d3b07e6dd7f2deabb18da907fc85074
Your Name <hiteshsachdeva343@gmail.com>
2025-10-29 15:43:05 -0400

    fix(ui): disable drag-and-drop for primitive source body nodes

--- packages/ui/src/components/Document/NodeContainer.tsx
+++ packages/ui/src/components/Document/NodeContainer.tsx
@@ -5,6 +5,7 @@ import { AddMappingNodeData, DocumentNodeData, NodeData } from '../../models/dat
 import { isDefined } from '../../utils';
 import './NodeContainer.scss';
 import { VisualizationService } from '../../services/visualization.service';
+import { DocumentType } from '../../models/datamapper/document';
 
 type DnDContainerProps = PropsWithChildren & {
   nodeData: NodeData;
@@ -78,15 +79,27 @@ type NodeContainerProps = PropsWithChildren & {
 
 export const NodeContainer = forwardRef<HTMLDivElement, NodeContainerProps>(
   ({ children, className, nodeData }, forwardedRef) => {
-    return nodeData &&
-      !(nodeData instanceof DocumentNodeData && !nodeData.isPrimitive) &&
-      !(nodeData instanceof AddMappingNodeData) ? (
+    if (!nodeData) {
+      return (
+        <div ref={forwardedRef} className={className}>
+          {children}
+        </div>
+      );
+    }
+
+    const isPrimitiveSourceBody =
+      nodeData.isPrimitive &&
+      nodeData instanceof DocumentNodeData &&
+      nodeData.document?.documentType === DocumentType.SOURCE_BODY;
+
+    const noDnD =
+      isPrimitiveSourceBody ||
+      nodeData instanceof AddMappingNodeData ||
+      (nodeData instanceof DocumentNodeData && !nodeData.isPrimitive);
+
+    return (
       <div ref={forwardedRef} className={className}>
-        <DnDContainer nodeData={nodeData}>{children}</DnDContainer>
-      </div>
-    ) : (
-      <div ref={forwardedRef} className={className}>
-        {children}
+        {!noDnD ? <DnDContainer nodeData={nodeData}>{children}</DnDContainer> : <>{children}</>}
       </div>
     );
   },
