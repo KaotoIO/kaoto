aa9e0f21691aac78e26e4ef83240c212d23a7423
Your Name <hiteshsachdeva343@gmail.com>
2025-10-29 14:43:51 -0400

    Fix: Prevent mapping of primitive Body without schema

--- packages/ui/src/providers/dnd/SourceTargetDnDHandler.ts
+++ packages/ui/src/providers/dnd/SourceTargetDnDHandler.ts
@@ -3,12 +3,23 @@ import { DragEndEvent, DragOverEvent, DragStartEvent } from '@dnd-kit/core';
 import { MappingTree } from '../../models/datamapper/mapping';
 import { NodeData } from '../../models/datamapper/visualization';
 import { VisualizationService } from '../../services/visualization.service';
+import { DocumentNodeData } from '../../models/datamapper/visualization';
+import { DocumentType } from '../../models/datamapper/document';
 
 export class SourceTargetDnDHandler implements DnDHandler {
   handleDragEnd(event: DragEndEvent, mappingTree: MappingTree, onUpdate: () => void): void {
     const fromNode = event.active.data.current as NodeData;
     const toNode = event.over?.data.current as NodeData;
     if (!fromNode || !toNode) return;
+    if (
+      fromNode.isSource &&
+      fromNode.isPrimitive &&
+      (fromNode as DocumentNodeData).document?.documentType === DocumentType.SOURCE_BODY
+    ) {
+      alert('Cannot map primitive source body without schema.');
+      return;
+    }
+
     const { sourceNode, targetNode } = VisualizationService.testNodePair(fromNode, toNode);
     if (sourceNode && targetNode) {
       VisualizationService.engageMapping(mappingTree, sourceNode, targetNode);
