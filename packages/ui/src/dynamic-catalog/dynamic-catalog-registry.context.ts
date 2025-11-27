ff671cc712a941f0c4c6a68b8a0cc3170253363a
Ricardo M. <lordrip@gmail.com>
2025-11-27 15:05:11 +0100

    chore(Catalog): Remove non-used DynamicCatalogRegistryProvider

--- packages/ui/src/dynamic-catalog/dynamic-catalog-registry.context.ts
+++
@@ -1,6 +0,0 @@
-import { createContext } from 'react';
-
-import { DynamicCatalogRegistry } from './dynamic-catalog-registry';
-import { IDynamicCatalogRegistry } from './models';
-
-export const DynamicCatalogRegistryContext = createContext<IDynamicCatalogRegistry>(DynamicCatalogRegistry.get());
--- packages/ui/src/dynamic-catalog/index.ts
+++ packages/ui/src/dynamic-catalog/index.ts
@@ -1,3 +1,1 @@
 export * from './dynamic-catalog-registry';
-export * from './dynamic-catalog-registry.context';
-export * from './dynamic-catalog-registry.provider';
