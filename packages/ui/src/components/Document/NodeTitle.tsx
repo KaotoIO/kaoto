ac29df2d2b5deae854364a25b4e760a5d94d2a44
PVinaches <palomavinaches@gmail.com>
2026-02-10 17:01:26 +0000

    feat(datamapper): field indicators icons added

--- packages/ui/src/components/Document/NodeTitle.tsx
+++ packages/ui/src/components/Document/NodeTitle.tsx
@@ -4,6 +4,9 @@ import { Label, Popover, Title } from '@patternfly/react-core';
 import clsx from 'clsx';
 import { FunctionComponent } from 'react';
 
+import optionalIcon from '../../assets/data-mapper/field-icons/optional-icon.svg';
+import repeat0Icon from '../../assets/data-mapper/field-icons/repeat0-icon.svg';
+import repeat1Icon from '../../assets/data-mapper/field-icons/repeat1-icon.svg';
 import {
   AddMappingNodeData,
   FieldItemNodeData,
@@ -41,12 +44,9 @@ export const NodeTitle: FunctionComponent<INodeTitle> = ({ className, rank, node
     nodeData instanceof FieldItemNodeData ||
     nodeData instanceof AddMappingNodeData
   ) {
-    const requiredField = nodeData.field.minOccurs !== 0;
-    const updatedContent = (
-      <span className={clsx('node-title__text', className)} data-rank={rank}>
-        {title} {requiredField && <span className="required-information">*</span>}
-      </span>
-    );
+    const optionalField = nodeData.field.minOccurs === 0;
+    const repeatingField0 = nodeData.field.minOccurs >= 0 && nodeData.field.maxOccurs === 'unbounded';
+    const repeatingField1 = nodeData.field.minOccurs >= 1 && nodeData.field.maxOccurs === 'unbounded';
 
     return (
       <Popover
@@ -66,7 +66,18 @@ export const NodeTitle: FunctionComponent<INodeTitle> = ({ className, rank, node
           </div>
         }
       >
-        {updatedContent}
+        <div className="node-title-container">
+          <span className={clsx('node-title__text', className)} data-rank={rank}>
+            {title}
+          </span>
+          {optionalField && !repeatingField0 && (
+            <img className="datamapper-marker-field" src={optionalIcon} alt="Optional" />
+          )}
+          {repeatingField0 && !repeatingField1 && (
+            <img className="datamapper-marker-field" src={repeat0Icon} alt="Repeat0" />
+          )}
+          {repeatingField1 && <img className="datamapper-marker-field" src={repeat1Icon} alt="Repeat1" />}
+        </div>
       </Popover>
     );
   }
