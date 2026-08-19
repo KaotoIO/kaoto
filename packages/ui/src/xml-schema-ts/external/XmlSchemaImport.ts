a749a6a5e4a9c85c82eda9cb05265964b2f3037c
Lars Heinemann <lhein.smx@gmail.com>
2026-08-19 12:51:35 +0200

    fix: restore constructors for XmlSchemaExternal subclasses with eslint suppression
    These constructors expose a protected parent constructor and cannot be removed.\nSuppress @typescript-eslint/no-useless-constructor on each with an inline comment.\n\nRelates to https://github.com/KaotoIO/kaoto/issues/3694

--- packages/ui/src/xml-schema-ts/external/XmlSchemaImport.ts
+++ packages/ui/src/xml-schema-ts/external/XmlSchemaImport.ts
@@ -4,6 +4,11 @@ import { XmlSchemaExternal } from './XmlSchemaExternal';
 export class XmlSchemaImport extends XmlSchemaExternal {
   namespace: string | null = null;
 
+  // eslint-disable-next-line @typescript-eslint/no-useless-constructor
+  constructor(parent: XmlSchema) {
+    super(parent);
+  }
+
   getNamespace(): string | null {
     return this.namespace;
   }
