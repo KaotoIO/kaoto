d5d26ad2b452e2e9e22367e13146c413fc6e7e49
Lars Heinemann <lhein.smx@gmail.com>
2026-08-28 21:12:38 +0200

    fix(test): add braces to waitFor arrow to satisfy no-confusing-void-expression
    The ESLint rule @typescript-eslint/no-confusing-void-expression forbids
    returning a void expression from an arrow shorthand. Add explicit braces.
    
    Relates to https://github.com/KaotoIO/kaoto/issues/3819

--- packages/ui/src/components/Visualization/Canvas/Form/CanvasFormBody.test.tsx
+++ packages/ui/src/components/Visualization/Canvas/Form/CanvasFormBody.test.tsx
@@ -412,7 +412,9 @@ describe('CanvasFormBody', () => {
     );
 
     const formPageObject = new KaotoFormPageObject(screen, act);
-    await waitFor(() => expect(formPageObject.getFieldByDisplayName('Name')).not.toBeNull());
+    await waitFor(() => {
+      expect(formPageObject.getFieldByDisplayName('Name')).not.toBeNull();
+    });
     const inputField = formPageObject.getFieldByDisplayName('Name')!;
 
     fireEvent.focus(inputField);
