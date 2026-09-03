1397826ed594bcb51866a51d3689c00dc81f1ec2
Chris <185133702+chrisriv10@users.noreply.github.com>
2026-09-03 16:43:22 -0500

    test: mock File.text directly

--- packages/ui/src/stubs/read-file-as-string.test.ts
+++ packages/ui/src/stubs/read-file-as-string.test.ts
@@ -1,28 +1,21 @@
 import { readFileAsString } from './read-file-as-string';
 
-// Local polyfill for Blob.text() / File.text() - not implemented in JSDOM
-// See https://github.com/jsdom/jsdom/issues/3405
-if (typeof Blob !== 'undefined' && !Blob.prototype.text) {
-  Blob.prototype.text = async function (this: Blob): Promise<string> {
-    return new Promise((resolve, reject) => {
-      const reader = new FileReader();
-      reader.onload = () => {
-        resolve(reader.result as string);
-      };
-      reader.onerror = () => {
-        reject(reader.error);
-      };
-      reader.readAsText(this);
-    });
-  };
-}
-
 describe('readFileAsString()', () => {
   it('should read', async () => {
     const file = new File(['foo'], 'foo.txt', {
       type: 'text/plain',
     });
+
+    // JSDOM doesn't implement File.text(), add it for testing
+    Object.defineProperty(file, 'text', {
+      value: vi.fn().mockResolvedValue('foo'),
+      writable: true,
+      configurable: true,
+    });
+
     const answer = await readFileAsString(file);
+
     expect(answer).toBe('foo');
+    expect(file.text).toHaveBeenCalledOnce();
   });
 });
