891a6e72cbbc750b1b78b91f66caaa04977be129
Ricardo M. <lordrip@gmail.com>
2026-09-08 18:20:23 +0200

    Remove dead KameletSchemaService.getKameletCatalogEntry method
    The method had no callers and used the synchronous CamelCatalogService,
    which is being phased out in favour of the async DynamicCatalogRegistry.
    Remove it entirely rather than migrating dead code, and strip the
    corresponding catalog setup scaffolding from the test file.
    
    Relates to https://github.com/KaotoIO/kaoto/issues/3886

--- packages/ui/src/models/visualization/flows/support/kamelet-schema.service.ts
+++ packages/ui/src/models/visualization/flows/support/kamelet-schema.service.ts
@@ -1,15 +1,6 @@
-import { IKameletDefinition } from '../../../camel/kamelets-catalog';
-import { CatalogKind } from '../../../catalog-kind';
-import { KameletBindingStep, PipeStep } from '../../../entities';
-import { CamelCatalogService } from '../camel-catalog.service';
+import { PipeStep } from '../../../entities';
 
 export class KameletSchemaService {
-  static getKameletCatalogEntry(step?: KameletBindingStep): IKameletDefinition | undefined {
-    const stepName = step?.ref?.name;
-
-    return CamelCatalogService.getComponent(CatalogKind.Kamelet, stepName);
-  }
-
   static getNodeLabel(step: PipeStep, path: string): string {
     const kameletName = step?.ref?.name;
 
